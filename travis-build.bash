57eed94a90779685cc361dec4078466ac1d1ee41
Alan Stewart <alankstewart@gmail.com>
2016-11-28 09:00:50 +1100

    Removed mvn -X

--- travis-build.bash
+++ travis-build.bash
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-mvn="mvn --settings .settings.xml -B -V -X"
+mvn="mvn --settings .settings.xml -B -V"
 if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     $mvn build-helper:parse-version versions:set -DnewVersion="$TRAVIS_TAG" versions:commit
     project_version="$TRAVIS_TAG"

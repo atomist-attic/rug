b699ea26a1042f38d59748b553dca8c758e44aa7
James Carnegie <james@atomist.com>
2016-12-01 21:43:40 +0000

    Yml type should support context free invocation

--- src/main/scala/com/atomist/rug/kind/yml/YmlType.scala
+++ src/main/scala/com/atomist/rug/kind/yml/YmlType.scala
@@ -1,6 +1,7 @@
 package com.atomist.rug.kind.yml
 
 import com.atomist.project.ProjectOperationArguments
+import com.atomist.rug.kind.dynamic.ContextlessViewFinder
 import com.atomist.rug.parser.Selected
 import com.atomist.rug.runtime.rugdsl.{DefaultEvaluator, Evaluator}
 import com.atomist.rug.spi.{MutableView, ReflectivelyTypedType, Type}
@@ -15,6 +16,7 @@ class YmlType(
                evaluator: Evaluator
              )
   extends Type(evaluator)
+    with ContextlessViewFinder
     with ReflectivelyTypedType {
 
   def this() = this(DefaultEvaluator)
@@ -33,4 +35,11 @@ class YmlType(
   //      .toSeq
   //  }
   override protected def findAllIn(rugAs: ArtifactSource, selected: Selected, context: MutableView[_], poa: ProjectOperationArguments, identifierMap: Map[String, Object]): Option[Seq[MutableView[_]]] = ???
+
+  /**
+    * The set of node types this can resolve from
+    *
+    * @return set of node types this can resolve from
+    */
+  override val resolvesFromNodeTypes: Set[String] = Set("project", "directory", "file")
 }

313c7830e3da985f54836e29346f12da2b4f25ef
Alan Stewart <alan@atomist.com>
2016-12-23 17:55:06 +1100

    Issue #62: cleanups

--- src/main/scala/com/atomist/rug/spi/ReflectiveFunctionExportingType.scala
+++ src/main/scala/com/atomist/rug/spi/ReflectiveFunctionExportingType.scala
@@ -8,26 +8,24 @@ import com.atomist.rug.runtime.rugdsl.{FunctionInvocationContext, RugFunction}
 import org.springframework.util.ReflectionUtils
 
 /**
-  * Export @ExportFunction methods
+  * Export @[[ExportFunction]] methods.
   */
 object ReflectiveFunctionExport {
 
   /**
-    * Export functions
+    * Export functions.
     */
   def exportedFunctions(c: Class[_]): Traversable[RugFunction[_, _]] = {
     val functions: Traversable[RugFunction[_, _]] =
       ReflectionUtils.getAllDeclaredMethods(c)
-        .filter(m => m.getAnnotations.exists(a => a.isInstanceOf[ExportFunction]))
-        .map(m => {
-          new ReflectiveRugFunction[FunctionTarget, Any](m)
-        })
+        .filter(_.getAnnotations.exists(_.isInstanceOf[ExportFunction]))
+        .map(new ReflectiveRugFunction[FunctionTarget, Any](_))
     functions
   }
 
   def exportedOperations(c: Class[_]): Seq[TypeOperation] = {
     ReflectionUtils.getAllDeclaredMethods(c)
-      .filter(m => m.getAnnotations.exists(a => a.isInstanceOf[ExportFunction]))
+      .filter(_.getAnnotations.exists(_.isInstanceOf[ExportFunction]))
       .map(m => {
         val a = m.getAnnotation(classOf[ExportFunction])
         val params = extractExportedParametersAndDocumentation(m)
@@ -37,21 +35,21 @@ object ReflectiveFunctionExport {
           m.getGenericReturnType.toString,
           m.getDeclaringClass,
           a.example() match {
-          case "" => None
-          case ex => Some(ex)
-        })
+            case "" => None
+            case ex => Some(ex)
+          })
       })
   }
 
   private def extractExportedParametersAndDocumentation(m: Method): Array[TypeParameter] = {
-    val params = m.getParameters.map({ p =>
+    val params = m.getParameters.map(p =>
       p.isAnnotationPresent(classOf[ExportFunctionParameterDescription]) match {
         case true => TypeParameter(p.getDeclaredAnnotation(classOf[ExportFunctionParameterDescription]).name(),
           p.getParameterizedType.toString,
           Some(p.getDeclaredAnnotation(classOf[ExportFunctionParameterDescription]).description()))
         case _ => TypeParameter(p.getName, p.getParameterizedType.toString, None)
       }
-    })
+    )
     params
   }
 
@@ -60,7 +58,7 @@ object ReflectiveFunctionExport {
   }
 }
 
-private class ReflectiveRugFunction[T <: FunctionTarget, R] (m: Method)
+private class ReflectiveRugFunction[T <: FunctionTarget, R](m: Method)
   extends RugFunction[T, R] {
 
   override def name: String = m.getName
@@ -82,33 +80,30 @@ private class ReflectiveRugFunction[T <: FunctionTarget, R] (m: Method)
     }
 
     try {
-      val result = if (ic.functionInvocation.pathBelow.isEmpty) {
-        // Simple invocation of the target method
-        val m2 = findMethod(ic.target, m.getName, ic.localArgs.length)
-        if(m2 == null){
-          val m3 =  findMethod(ic.target, m.getName)
-          m3.invoke(ic.target, argsToUse: _*)
-        }else{
-          m2.invoke(ic.target, ic.localArgs: _*)
+      val result =
+        if (ic.functionInvocation.pathBelow.isEmpty) {
+          // Simple invocation of the target method
+          val m2 = findMethod(ic.target, m.getName, ic.localArgs.length)
+          if (m2 == null) {
+            val m3 = findMethod(ic.target, m.getName)
+            m3.invoke(ic.target, argsToUse: _*)
+          } else {
+            m2.invoke(ic.target, ic.localArgs: _*)
+          }
+        } else {
+          var target = m.invoke(ic.target)
+          for (pe <- ic.functionInvocation.pathBelow.dropRight(1)) {
+            val m2 = findMethod(target, pe)
+            target = m2.invoke(target)
+          }
+          val m2 = findMethod(target, ic.functionInvocation.pathBelow.last)
+          m2.invoke(target, argsToUse: _*)
         }
-
-      }
-      else {
-        var target = m.invoke(ic.target)
-        for (pe <- ic.functionInvocation.pathBelow.dropRight(1)) {
-          val m2 = findMethod(target, pe)
-          target = m2.invoke(target)
-        }
-        val m2 = findMethod(target, ic.functionInvocation.pathBelow.last)
-        m2.invoke(target, argsToUse: _*)
-      }
       result.asInstanceOf[R]
-    }
-    catch {
+    } catch {
       case iae: IllegalArgumentException =>
         throw new RugRuntimeException(name,
-          s"Encountered '${iae.getMessage}' trying to invoke $m with [${argsToUse.mkString(",")}] on ${ic.target}",
-          iae)
+          s"Encountered '${iae.getMessage}' trying to invoke $m with [${argsToUse.mkString(",")}] on ${ic.target}", iae)
       case ite: InvocationTargetException =>
         ite.getTargetException match {
           case f: InstantEditorFailureException =>
@@ -123,16 +118,12 @@ private class ReflectiveRugFunction[T <: FunctionTarget, R] (m: Method)
 
   private def findMethod(target: Object, name: String): Method = {
     try {
-      ReflectionUtils.getAllDeclaredMethods(target.getClass).find(m =>
-        m.getName.equals(name)
-      )
-        .getOrElse(
-          throw new RugRuntimeException(null, s"Unknown method '$name' on ${target.getClass}")
-        )
-    }
-    catch {
-      case nsme: NoSuchMethodException =>
-        throw new RugRuntimeException(null, s"Unknown method '$name' on ${target.getClass}", nsme)
+      ReflectionUtils.getAllDeclaredMethods(target.getClass)
+        .find(_.getName.equals(name))
+        .getOrElse(throw new RugRuntimeException(null, s"Unknown method '$name' on ${target.getClass}"))
+    } catch {
+      case e: NoSuchMethodException =>
+        throw new RugRuntimeException(null, s"Unknown method '$name' on ${target.getClass}", e)
     }
   }
 
@@ -141,12 +132,11 @@ private class ReflectiveRugFunction[T <: FunctionTarget, R] (m: Method)
       ReflectionUtils.getAllDeclaredMethods(target.getClass).find(m =>
         m.getName.equals(name) && m.getParameterCount == num_params
       ).orNull
-    }
-    catch {
-      case nsme: NoSuchMethodException =>
-        throw new RugRuntimeException(null, s"Unknown method '$name' on ${target.getClass}", nsme)
+    } catch {
+      case e: NoSuchMethodException =>
+        throw new RugRuntimeException(null, s"Unknown method '$name' on ${target.getClass}", e)
     }
   }
 
-  override def toString = name
+  override def toString: String = name
 }

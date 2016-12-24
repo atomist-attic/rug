5b5e8622dc1eba1359912377270b5ca2aa593242
Rod Johnson <rod@atomist.com>
2016-12-24 09:45:06 -0800

    Beginning to integrate microgrammar support with TypeScript. Improved comments in TypeScript files

--- src/main/scala/com/atomist/rug/runtime/js/interop/jsPathExpressionEngine.scala
+++ src/main/scala/com/atomist/rug/runtime/js/interop/jsPathExpressionEngine.scala
@@ -8,6 +8,7 @@ import com.atomist.rug.kind.DefaultTypeRegistry
 import com.atomist.rug.kind.dynamic.ContextlessViewFinder
 import com.atomist.rug.spi._
 import com.atomist.tree.TreeNode
+import com.atomist.tree.content.text.microgrammar.{MatcherDSLDefinitionParser, MatcherMicrogrammar, MicrogrammarTypeProvider}
 import com.atomist.tree.pathexpression.{ExpressionEngine, PathExpressionEngine, PathExpressionParser}
 import com.atomist.util.lang.TypeScriptArray
 import jdk.nashorn.api.scripting.ScriptObjectMirror
@@ -35,10 +36,35 @@ case class Match(root: Object, matches: _root_.java.util.List[Object])
   * @see ExpressionEngine
   * @param ee underlying ExpressionEngine that does the actual work
   */
-class jsPathExpressionEngine(val ee: ExpressionEngine = new PathExpressionEngine) {
+class jsPathExpressionEngine(
+                              val ee: ExpressionEngine = new PathExpressionEngine,
+                              typeRegistry: TypeRegistry = DefaultTypeRegistry) {
 
-  // TypeRegistry shared among all users
-  private val sharedTypeRegistry: TypeRegistry = DefaultTypeRegistry
+  /**
+    * Return a customized version of this path expression engine for use in a specific
+    * context, with its own microgrammar types
+    * @param mg microgrammar objects
+    * @return
+    */
+  def customize(mg: String): jsPathExpressionEngine = {
+    val things = Seq(mg)
+    val tr = new UsageSpecificTypeRegistry(this.typeRegistry,
+      things.map(dynamicTypeDefinitionToTypeProvider(_))
+    )
+    new jsPathExpressionEngine(this.ee, tr)
+  }
+
+  private def dynamicTypeDefinitionToTypeProvider(o: Object): Typed = o match {
+    case som: ScriptObjectMirror =>
+      // It's a microgrammar
+      val name = NashornUtils.stringProperty(som, "name")
+      val grammar = NashornUtils.stringProperty(som, "grammar")
+      if (name == null || grammar == null)
+        throw new RugRuntimeException(null, "A microgrammar must specify both name and grammar")
+      val parsed = jsPathExpressionEngine.matcherParser.parse(grammar)
+      val mg = new MatcherMicrogrammar(name, parsed)
+      new MicrogrammarTypeProvider(mg)
+  }
 
   /**
     * Evaluate the given path expression.
@@ -50,14 +76,14 @@ class jsPathExpressionEngine(val ee: ExpressionEngine = new PathExpressionEngine
     *             The latter allows us to define TypeScript classes.
     * @return a Match
     */
-  def evaluate(root: Object, pe: Object /*, microgrammars: Object*/): Match = {
+  def evaluate(root: Object, pe: Object): Match = {
     pe match {
       case som: ScriptObjectMirror =>
         // Examine a JavaScript object passed to us. It's probably a
         // TypeScript class with an "expression" property
         val expr = NashornUtils.stringProperty(som, "expression")
         val parsed = PathExpressionParser.parsePathExpression(expr)
-        ee.evaluate(toTreeNode(root), parsed, sharedTypeRegistry) match {
+        ee.evaluate(toTreeNode(root), parsed, typeRegistry) match {
           case Right(nodes) =>
             val m = Match(root, wrap(nodes))
             m
@@ -66,7 +92,7 @@ class jsPathExpressionEngine(val ee: ExpressionEngine = new PathExpressionEngine
         }
       case expr: String =>
         val parsed = PathExpressionParser.parsePathExpression(expr)
-        ee.evaluate(toTreeNode(root), parsed, sharedTypeRegistry) match {
+        ee.evaluate(toTreeNode(root), parsed, typeRegistry) match {
           case Right(nodes) =>
             val m = Match(root, wrap(nodes))
             m
@@ -130,7 +156,7 @@ class jsPathExpressionEngine(val ee: ExpressionEngine = new PathExpressionEngine
     */
   def children(parent: Object, name: String): util.List[Object] = {
     val rootTn = toTreeNode(parent)
-    val typ = sharedTypeRegistry.findByName(name).getOrElse(???)
+    val typ = typeRegistry.findByName(name).getOrElse(???)
     (typ, rootTn) match {
       case (cvf: ContextlessViewFinder, mv: MutableView[_]) =>
         val kids = cvf.findAllIn(mv).getOrElse(Nil)
@@ -148,10 +174,16 @@ class jsPathExpressionEngine(val ee: ExpressionEngine = new PathExpressionEngine
     */
   def wrap(nodes: Seq[TreeNode]): java.util.List[Object] = {
     new TypeScriptArray(nodes.map(k => new SafeCommittingProxy({
-      sharedTypeRegistry.findByName(k.nodeType).getOrElse(
+      typeRegistry.findByName(k.nodeType).getOrElse(
         throw new UnsupportedOperationException(s"Cannot find type for node type [${k.nodeType}]")
       )
     },
       k).asInstanceOf[Object]).asJava)
   }
+}
+
+object jsPathExpressionEngine {
+
+  val matcherParser = new MatcherDSLDefinitionParser
+
 }
--- src/test/scala/com/atomist/tree/content/text/microgrammar/TypeScriptMicrogrammarTest.scala
+++ src/test/scala/com/atomist/tree/content/text/microgrammar/TypeScriptMicrogrammarTest.scala
@@ -3,57 +3,52 @@ package com.atomist.tree.content.text.microgrammar
 import com.atomist.parse.java.ParsingTargets
 import com.atomist.project.edit.SuccessfulModification
 import com.atomist.project.{ProjectOperation, SimpleProjectOperationArguments}
-import com.atomist.rug.InvalidRugParameterPatternException
+import com.atomist.rug.{InvalidRugParameterPatternException, TestUtils}
 import com.atomist.rug.runtime.js.{JavaScriptInvokingProjectEditor, JavaScriptOperationFinder}
 import com.atomist.source.{FileArtifact, SimpleFileBasedArtifactSource, StringFileArtifact}
 import org.scalatest.{FlatSpec, Matchers}
 
-abstract class TypeScriptMicrogrammarTest extends FlatSpec with Matchers {
-
-  //  val findFile = "/*:File[name='pom.xml']"
-  //  val mg: Microgrammar = new MatcherMicrogrammar("modelVersion",
-  //    mgp.parse("<modelVersion>$modelVersion:§[a-zA-Z0-9_\\.]+§</modelVersion>"))
+class TypeScriptMicrogrammarTest extends FlatSpec with Matchers {
 
   val ModifiesWithSimpleMicrogrammar: String =
     """import {Project} from '@atomist/rug/model/Core'
       |import {ProjectEditor} from '@atomist/rug/operations/ProjectEditor'
-      |import {PathExpression,TreeNode} from '@atomist/rug/tree/PathExpression'
+      |import {PathExpression,TreeNode,Microgrammar} from '@atomist/rug/tree/PathExpression'
       |import {PathExpressionEngine} from '@atomist/rug/tree/PathExpression'
       |import {Match} from '@atomist/rug/tree/PathExpression'
       |import {File} from '@atomist/rug/model/Core'
       |import {Result,Status, Parameter} from '@atomist/rug/operations/RugOperation'
       |
-      |class ConstructedEditor implements ProjectEditor {
+      |class MgEditor implements ProjectEditor {
       |    name: string = "Constructed"
       |    description: string = "A nice little editor"
-      |    tags: string[] = ["java", "maven"]
       |
       |    edit(project: Project) {
       |
-      |      let eng: PathExpressionEngine = project.context().pathExpressionEngine();
+      |      let mg = new Microgrammar('modelVersion', `<modelVersion>$modelVersion:§[a-zA-Z0-9_\\.]+§</modelVersion>`)
+      |      let eng: PathExpressionEngine = project.context().pathExpressionEngine()//.customize(mg)
       |
       |      var t: string = `filecount=${project.fileCount()}`
       |
       |      eng.with<TreeNode>(project, "/*[@name='pom.xml']/modelVersion()", n => {
-      |        t += `Matched file=${n.value()}`;
-      |        //n.append("randomness")
+      |        console.log(`Matched file=${n.value()}`);
+      |        //n.update('Foo bar')
       |      })
       |
-      |        return new Result(Status.Success,
-      |        `${t}\n\nEdited Project containing ${project.fileCount()} files`)
+      |        return new Result(Status.Success, `OK`)
       |    }
       |  }
-      |  var editor = new ConstructedEditor()
+      |  var editor = new MgEditor()
       | """.stripMargin
 
 
-  it should "run use microgrammar defined in TypeScript" in {
+  it should "run use microgrammar defined in TypeScript" in pendingUntilFixed {
     invokeAndVerifySimple(StringFileArtifact(s".atomist/editors/SimpleEditor.ts",
       ModifiesWithSimpleMicrogrammar))
   }
 
   private def invokeAndVerifySimple(tsf: FileArtifact, others: Seq[ProjectOperation] = Nil): JavaScriptInvokingProjectEditor = {
-    val as = SimpleFileBasedArtifactSource(tsf)
+    val as = TestUtils.compileWithModel(SimpleFileBasedArtifactSource(tsf))
     val jsed = JavaScriptOperationFinder.fromJavaScriptArchive(as).head.asInstanceOf[JavaScriptInvokingProjectEditor]
     val target = ParsingTargets.NewStartSpringIoProject
     jsed.modify(target, SimpleProjectOperationArguments("", Map("content" -> "Anders Hjelsberg is God"))) match {

c0ba1db875b08105c67243b7a11a4b2651eeff24
Rod Johnson <rod@atomist.com>
2016-12-27 09:27:48 -0800

    Pending test

--- src/test/scala/com/atomist/tree/content/text/microgrammar/dsl/MatcherDefinitionUsageTest.scala
+++ src/test/scala/com/atomist/tree/content/text/microgrammar/dsl/MatcherDefinitionUsageTest.scala
@@ -50,7 +50,7 @@ class MatcherDefinitionUsageTest extends FlatSpec with Matchers {
   it should "parse Slack emoji html" in {
     val html =
       """<tr class="emoji_row">
-        |													<td class="align_middle"><span data-original="https://emoji.slack-edge.com/T024F4A92/666/5b9d8b4d571e51c5.jpg" class="lazy emoji-wrapper"></span></td>
+        |													<td class="align_middle"><span data-original="https://emoji.slack-edge.com/T024F4A92/666/5b9d8b4d571e51c5.jpg" class="lazyemoji-wrapper"></span></td>
         |												<td class="align_middle" style="white-space: normal; word-break: break-all; word-wrap: break-word; height: 100%; vertical-align:middle;">:666:
         |												</td>
         |													<td class="align_middle">Image</td>
@@ -67,7 +67,9 @@ class MatcherDefinitionUsageTest extends FlatSpec with Matchers {
 
     val mg = new MatcherMicrogrammar(
       mgp.parseMatcher("emoji",
-        """<tr class="emoji_row">¡<span data-original="¡$emojiUrl:§https://[a-zA_Z\.\-_0-9]+§" class="$name:§[a-zA-Z\S0-9_\-]*§"""))
+        """<tr class="emoji_row">¡<span data-original="¡$emojiUrl:§https://[a-zA_Z\.\-_0-9]+§"""
+        //"""<tr class="emoji_row">¡<span data-original="¡$emojiUrl:§https://[a-zA_Z\.\-_0-9]+§" class="$name:§[a-zA-Z0-9_\-]*§"""
+      ))
     println(mg)
 
     val matches = mg.findMatches(html)

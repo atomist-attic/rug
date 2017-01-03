d935a119da1eba63ead5cbe6339ad9cad4668353
James Carnegie <james@atomist.com>
2017-01-03 11:21:14 +0000

    Ensure that line markers are ignored when parsing key=value pairs #140

--- src/test/scala/com/atomist/rug/kind/docker/DockerfileParserTest.scala
+++ src/test/scala/com/atomist/rug/kind/docker/DockerfileParserTest.scala
@@ -27,4 +27,10 @@ class DockerfileParserTest extends FlatSpec with Matchers {
     val parsed = parse("# test\nFROM java:8-jre\n\nRUN test \\ \n\ttest1 \\ \n\ttest2\n\nADD test.txt test.txtww\n\nEXPOSE 8080\nEXPOSE 8090").toString
     parsed should equal("# test\nFROM java:8-jre\n\nRUN test \\\n\ttest1 \\\n\ttest2\n\nADD test.txt test.txtww\n\nEXPOSE 8080\nEXPOSE 8090".replaceAll("\\n",System.lineSeparator()))
   }
+
+  it should "parse a docker file with multiple lines in the LABEL section" in {
+    val parsed = parse("FROM python:3.5\n\nLABEL com.atomist.line1=\"line1\" \\\n      com.atomist.line2=\"line2\"\n\nRUN test \\\n\ttest1 \\\n\ttest2\n\nENTRYPOINT python -v").toString
+    parsed should include("com.atomist.line1=\"line1\"")
+    parsed should include("com.atomist.line2=\"line2\"")
+  }
 }

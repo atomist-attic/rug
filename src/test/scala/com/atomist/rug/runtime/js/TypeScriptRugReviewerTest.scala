26a971e774710553629ad3a25231d22edce9a627
Russ Miles <russ@atomist.com>
2017-01-06 16:03:40 +0000

    More testing around TypeScript reviewer, exploring the mapping to the ReviewResult object
    Contributes to: #143

--- src/test/scala/com/atomist/rug/runtime/js/TypeScriptRugReviewerTest.scala
+++ src/test/scala/com/atomist/rug/runtime/js/TypeScriptRugReviewerTest.scala
@@ -13,6 +13,8 @@ object TypeScriptRugReviewerTest {
 
   val compiler = new TypeScriptCompiler()
 
+  val ParameterContent = "Anders Hjelsberg is God"
+
   val SimpleReviewerWithoutParameters =
     """
       |import {Project} from '@atomist/rug/model/Core'
@@ -30,15 +32,42 @@ object TypeScriptRugReviewerTest {
       |var reviewer = new SimpleReviewer()
     """.stripMargin
 
+  val SimpleReviewerWithParameters =
+    s"""
+       |import {Project} from '@atomist/rug/model/Core'
+       |import {ProjectReviewer} from '@atomist/rug/operations/ProjectReviewer'
+       |import {File} from '@atomist/rug/model/Core'
+       |import {ReviewResult, ReviewComment, Parameter} from '@atomist/rug/operations/RugOperation'
+       |
+       |class SimpleReviewer implements ProjectReviewer {
+       |    name: string = "Simple"
+       |    description: string = "A nice little reviewer"
+       |    parameters: Parameter[] = [{name: "content", description: "Content", pattern: "@url", maxLength: 100}]
+       |    review(project: Project, {content} : {content: string}) {
+       |      //p["otherParam"] = p.content
+       |      return new ReviewResult(content,
+       |          <ReviewComment[]>[]
+       |        );
+       |    }
+       |  }
+       |var reviewer = new SimpleReviewer()
+    """.stripMargin
 }
 
 class TypeScriptRugReviewerTest extends FlatSpec with Matchers {
   import TypeScriptRugReviewerTest._
 
-  it should "run simple reviewer compiled from TypeScript without parameters using support class" in {
+  it should "run simple reviewer compiled from TypeScript without parameters" in {
     val reviewResult = invokeAndVerifySimple(StringFileArtifact(s".atomist/reviewers/SimpleReviewer.ts", SimpleReviewerWithoutParameters))
 
     reviewResult.note should be("")
+    reviewResult.comments.isEmpty should be(true)
+  }
+
+  it should "run simple reviewer compiled from TypeScript with parameters" in {
+    val reviewResult = invokeAndVerifySimple(StringFileArtifact(s".atomist/reviewers/SimpleReviewer.ts", SimpleReviewerWithParameters))
+
+    reviewResult.note should be(ParameterContent)
     reviewResult.comments.isEmpty should be(true)
   }
 
@@ -51,6 +80,6 @@ class TypeScriptRugReviewerTest extends FlatSpec with Matchers {
 
     val target = SimpleFileBasedArtifactSource(StringFileArtifact("pom.xml", "nasty stuff"))
 
-    jsed.review(target, SimpleProjectOperationArguments("", Map("content" -> "Anders Hjelsberg is God")))
+    jsed.review(target, SimpleProjectOperationArguments("", Map("content" -> ParameterContent)))
   }
 }

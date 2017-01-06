b3e95bada6a3b2b525605b3d999c5a8a9d506b67
Rod Johnson <rod@atomist.com>
2017-01-05 16:29:07 -0800

    Added test for PathExpression object. Added comments to TypeScript and removed obsolete class

--- src/test/scala/com/atomist/rug/runtime/js/TypeScriptRugEditorTest.scala
+++ src/test/scala/com/atomist/rug/runtime/js/TypeScriptRugEditorTest.scala
@@ -138,6 +138,48 @@ object TypeScriptRugEditorTest {
        |var myeditor = new SimpleEditor()
     """.stripMargin
 
+    val EditorInjectedWithPathExpressionObject: String =
+      """import {Project} from '@atomist/rug/model/Core'
+        |import {ProjectEditor} from '@atomist/rug/operations/ProjectEditor'
+        |import {PathExpression} from '@atomist/rug/tree/PathExpression'
+        |import {PathExpressionEngine} from '@atomist/rug/tree/PathExpression'
+        |import {Match} from '@atomist/rug/tree/PathExpression'
+        |import {File} from '@atomist/rug/model/Core'
+        |import {Result,Status, Parameter} from '@atomist/rug/operations/RugOperation'
+        |
+        |class PomFile extends PathExpression<Project,File> {
+        |
+        |    constructor() { super(`/File()[@name='pom.xml']`) }
+        |}
+        |
+        |class ConstructedEditor implements ProjectEditor {
+        |
+        |    name: string = "Constructed"
+        |    description: string = "A nice little editor"
+        |    tags: string[] = ["java", "maven"]
+        |    parameters: Parameter[] = [{name: "packageName", description: "The Java package name", displayName: "Java Package", pattern: "^.*$", maxLength: 100}]
+        |    edit(project: Project, {packageName } : {packageName: string}) {
+        |
+        |      let eng: PathExpressionEngine = project.context().pathExpressionEngine();
+        |      let m = eng.evaluate<Project,File>(project, new PomFile())
+        |
+        |      var t: string = `param=${packageName},filecount=${m.root().fileCount()}`
+        |      for (let n of m.matches()) {
+        |        t += `Matched file=${n.path()}`;
+        |        n.append("randomness")
+        |        }
+        |
+        |        var s: string = ""
+        |        project.addFile("src/from/typescript", "Anders Hjelsberg is God");
+        |        for (let f of project.files())
+        |            s = s + `File [${f.path()}] containing [${f.content()}]\n`
+        |        return new Result(Status.Success,
+        |        `${t}\n\nEdited Project containing ${project.fileCount()} files: \n${s}`)
+        |    }
+        |  }
+        |  var editor = new ConstructedEditor()
+        | """.stripMargin
+
   val EditorInjectedWithPathExpression: String =
     """import {Project} from '@atomist/rug/model/Core'
       |import {ProjectEditor} from '@atomist/rug/operations/ProjectEditor'
@@ -321,6 +363,12 @@ class TypeScriptRugEditorTest extends FlatSpec with Matchers {
   it should "have the PathExpressionEngine injected" in {
     val ed = invokeAndVerifyConstructed(StringFileArtifact(s".atomist/editors/ConstructedEditor.ts",
       EditorInjectedWithPathExpression))
+    ed.description should be ("A nice little editor")
+  }
+
+  it should "have the PathExpressionEngine injected and use an object path expression" in {
+    val ed = invokeAndVerifyConstructed(StringFileArtifact(s".atomist/editors/ConstructedEditor.ts",
+      EditorInjectedWithPathExpressionObject))
     ed.description should be ("A nice little editor")
   }
 

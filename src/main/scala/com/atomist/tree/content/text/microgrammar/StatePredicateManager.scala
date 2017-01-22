3cdd2da36adc313d3ea52f83a3bb22fa012a85a1
Rod Johnson <rod@atomist.com>
2017-01-22 13:14:06 -0800

    Microgrammar predicates are now immutable

--- src/main/scala/com/atomist/tree/content/text/microgrammar/StatePredicateManager.scala
+++ src/main/scala/com/atomist/tree/content/text/microgrammar/StatePredicateManager.scala
@@ -5,19 +5,26 @@ import com.atomist.util.lang.JavaHelpers
 /**
   * Manages a set of StatePredicates
   */
-class StatePredicateManager extends InputConsumer {
+case class StatePredicateManager(
+                                  predicates: Map[String, StatePredicate[_]] = Map(),
+                                  offset: Int = 0)
+  extends InputConsumer {
 
   import StatePredicateManager._
 
-  private var predicates: Map[String, StatePredicate[_]] = Map()
-
-  def register(p: StatePredicate[_]): Unit = predicates += (nameFor(p) -> p)
+  def register(p: StatePredicate[_]): StatePredicateManager =
+    StatePredicateManager(predicates ++ Map(nameFor(p) -> p))
 
   def registeredPredicates: Set[String] = predicates.keySet
 
   def registered(predicateName: String): Boolean = predicates.contains(predicateName)
 
-  override def consume(c: Char): Unit = predicates.values.foreach(_.consume(c))
+  override def consume(c: Char): StatePredicateManager = {
+    val updatedPredicates: Map[String, StatePredicate[_]] = predicates.map {
+      case (k, v) => (k, v.consume(c))
+    }.toMap
+    StatePredicateManager(updatedPredicates, offset + 1)
+  }
 
   def valueOf[R](predicateName: String): Option[R] = predicates.get(predicateName).map(p => p.state) match {
     case or: Option[R @unchecked] => or

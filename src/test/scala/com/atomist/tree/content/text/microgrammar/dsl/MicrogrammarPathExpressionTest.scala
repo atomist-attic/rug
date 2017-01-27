7649fa40fcc250dac8ca8ca53952b388925ab92a
Jessica Kerr <jessitron@atomist.com>
2017-01-27 14:57:11 -0600

    Refactor test

--- src/test/scala/com/atomist/tree/content/text/microgrammar/dsl/MicrogrammarPathExpressionTest.scala
+++ src/test/scala/com/atomist/tree/content/text/microgrammar/dsl/MicrogrammarPathExpressionTest.scala
@@ -5,7 +5,8 @@ import com.atomist.rug.kind.DefaultTypeRegistry
 import com.atomist.rug.kind.core.ProjectMutableView
 import com.atomist.rug.spi.UsageSpecificTypeRegistry
 import com.atomist.source.{SimpleFileBasedArtifactSource, StringFileArtifact}
-import com.atomist.tree.content.text.microgrammar.{Literal, MatcherMicrogrammar, MicrogrammarTypeProvider}
+import com.atomist.tree.TreeNode
+import com.atomist.tree.content.text.microgrammar._
 import com.atomist.tree.pathexpression.{PathExpressionEngine, PathExpressionParser}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -13,20 +14,17 @@ import org.scalatest.{FlatSpec, Matchers}
 class OptionalFieldMicrogrammarTest extends FlatSpec with Matchers {
   it should "Let me access something that might exist but does not" in pending
 
-  it should "match an unnamed literal" in {
-    val input =
-      """There was a banana. It crossed the street. A car ran over it.
-        |No banana for you.
-        |""".stripMargin
+
+  def exercisePathExpression(microgrammar: Microgrammar, pathExpressionString: String, input: String): List[TreeNode] = {
+
+    /* Construct a root node */
     val as = SimpleFileBasedArtifactSource(StringFileArtifact("banana.txt", input))
-    val pmv = new ProjectMutableView(as, as, DefaultAtomistConfig)
+    val pmv = new ProjectMutableView(as /* cheating */, as, DefaultAtomistConfig)
 
-    val microgrammar =
-      new MatcherMicrogrammar(
-        Literal("banana")  // another test needs: Literal("a ) ~ Regex("[a-z]+", "blah") ~ Optional(Literal("yo", Some("myWord"))) ~ Literal(".")
-        , "bananagrammar")  
-    val pathExpression = PathExpressionParser.parseString("/File()/bananagrammar()")
+    /* Parse the path expression */
+    val pathExpression = PathExpressionParser.parseString(pathExpressionString)
 
+    /* Install the microgrammar */
     val typeRegistryWithMicrogrammar =
       new UsageSpecificTypeRegistry(DefaultTypeRegistry,
         Seq(new MicrogrammarTypeProvider(microgrammar)))
@@ -35,8 +33,22 @@ class OptionalFieldMicrogrammarTest extends FlatSpec with Matchers {
 
     result match {
       case Left(a) => fail(a)
-      case Right(b) => b.size should be(2)
+      case Right(b) => b
     }
+  }
 
+  val input: String =
+    """There was a banana. It crossed the street. A car ran over it.
+      |No banana for you.
+      |""".stripMargin
+
+  it should "match an unnamed literal" in {
+
+    val microgrammar = new MatcherMicrogrammar(Literal("banana"), "bananagrammar")
+    val pathExpression = "/File()/bananagrammar()"
+
+    val result = exercisePathExpression(microgrammar, pathExpression, input)
+
+    result.size should be(2)
   }
 }

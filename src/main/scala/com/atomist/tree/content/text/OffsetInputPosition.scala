6cfbb7a39f80c9941a84ffd4d8388860992f51f5
Rod Johnson <rod@atomist.com>
2017-01-31 13:17:01 -0800

    Introduced FormatInfo for position calculations

---
+++ src/main/scala/com/atomist/tree/content/text/OffsetInputPosition.scala
@@ -0,0 +1,17 @@
+package com.atomist.tree.content.text
+
+case class OffsetInputPosition(offset: Int) extends InputPosition {
+
+  require(offset >= 0, s"Offset must be >= 0, had $offset")
+
+  override def show: String = s"offset=$offset"
+
+  override def toString: String = show
+}
+
+object OffsetInputPosition {
+
+  def startOf(s: String) = LineHoldingOffsetInputPosition(s, 0)
+
+  def endOf(s: String) = LineHoldingOffsetInputPosition(s, s.length)
+}

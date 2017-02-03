33ef3dc5f8a3a57349769b60ff71a9f72fc837e5
Rod Johnson <rod@atomist.com>
2017-02-02 21:21:56 -0800

    Added tests for JavaHelpers

--- src/test/scala/com/atomist/util/lang/JavaHelpersTest.scala
+++ src/test/scala/com/atomist/util/lang/JavaHelpersTest.scala
@@ -28,4 +28,39 @@ class JavaHelpersTest extends FlatSpec with Matchers {
       isValidPackageName(p) should be (false)
   }
 
+  it should "convert to Java variable name" in {
+    val s = "this_is_a_thing"
+    toJavaVariableName(s) should equal ("thisIsAThing")
+  }
+
+  it should "camelize name" in {
+    val s = "this_is_a_thing"
+    toCamelizedPropertyName(s) should equal ("thisIsAThing")
+  }
+
+  it should "camelize name with $" in {
+    val s = "t$his_is_a_thing"
+    toCamelizedPropertyName(s) should equal ("t$hisIsAThing")
+  }
+
+  it should "uncamelize" in {
+    val s = "thisIsAThing"
+    toLowerCaseDelimited(s) should equal ("this_is_a_thing")
+  }
+
+  it should "property-ize" in {
+    val s = "rug"
+    propertyNameToGetterName(s) should equal ("getRug")
+  }
+
+  it should "upperize" in {
+    val tests = Seq("a", "aa", "dude", "duder", "el duderino", "his dudeness")
+    for (t <- tests) {
+      val r = upperize(t)
+      r.charAt(0).isUpper should be (true)
+      r.substring(1) should equal (t.substring(1))
+    }
+
+  }
+
 }

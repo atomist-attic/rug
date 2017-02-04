a467083a26cf77d81a1d48f669769ed39499e5b5
Rod Johnson <rod@atomist.com>
2017-02-05 09:12:13 +1100

    Extracted RugType into its own file

--- src/main/scala/com/atomist/rug/kind/rug/dsl/RugType.scala
+++ src/main/scala/com/atomist/rug/kind/rug/dsl/RugType.scala
@@ -1,40 +1,12 @@
 package com.atomist.rug.kind.rug.dsl
 
-import com.atomist.project.ProjectOperationArguments
-import com.atomist.rug.kind.core.{DirectoryMutableView, FileArtifactBackedMutableView, ProjectMutableView}
 import com.atomist.rug.kind.grammar.AntlrRawFileType
-import com.atomist.rug.parser.Selected
-import com.atomist.rug.runtime.rugdsl.Evaluator
-import com.atomist.rug.spi.{MutableView, ReflectivelyTypedType, Type}
-import com.atomist.source.{ArtifactSource, FileArtifact}
-import com.atomist.tree.TreeNode
-import com.atomist.tree.content.text.grammar.antlr.{FromGrammarAstNodeCreationStrategy}
+import com.atomist.source.FileArtifact
+import com.atomist.tree.content.text.grammar.antlr.FromGrammarAstNodeCreationStrategy
 
-class EditorType(evaluator: Evaluator) extends Type(evaluator) with ReflectivelyTypedType {
-  /** Describe the MutableView subclass to allow for reflective function export */
-
-  override def viewManifest: Manifest[_] = manifest[EditorMutableView]
-
-  override protected def findAllIn(rugAs: ArtifactSource,
-                                   selected: Selected,
-                                   context: TreeNode,
-                                   poa: ProjectOperationArguments,
-                                   identifierMap: Map[String, Object]): Option[Seq[MutableView[_]]] =
-    context match {
-      case pmv: ProjectMutableView => ???
-      case f: FileArtifactBackedMutableView =>
-        Some(Seq(new EditorMutableView(f.currentBackingObject, f.parent)))
-      case d: DirectoryMutableView => ???
-      case _ => None
-    }
-
-  /**
-    * Description of this type
-    */
-  override def description: String = "Rug Editor, in Rug DSL or typescript"
-
-}
-
+/**
+  * Exposes an ANTLR-based AST for Rug DSL files.
+  */
 class RugFileType
   extends AntlrRawFileType(
     topLevelProduction = "rug_file",

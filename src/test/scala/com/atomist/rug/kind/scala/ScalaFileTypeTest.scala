b3f70033c93699f44ff5430e46df5ca4c306629f
Rod Johnson <rod@atomist.com>
2017-02-08 12:13:41 +1100

    Improved ScalaFileTypeTest

--- src/test/scala/com/atomist/rug/kind/scala/ScalaFileTypeTest.scala
+++ src/test/scala/com/atomist/rug/kind/scala/ScalaFileTypeTest.scala
@@ -3,35 +3,33 @@ package com.atomist.rug.kind.scala
 import com.atomist.rug.kind.DefaultTypeRegistry
 import com.atomist.rug.kind.core.ProjectMutableView
 import com.atomist.rug.kind.dynamic.MutableContainerMutableView
+import com.atomist.rug.kind.grammar.AbstractTypeUnderFileTest
 import com.atomist.source.{EmptyArtifactSource, SimpleFileBasedArtifactSource, StringFileArtifact}
-import com.atomist.tree.{MutableTreeNode, TreeNode}
 import com.atomist.tree.content.text.{ConsoleMatchListener, TreeNodeOperations}
-import com.atomist.tree.pathexpression.{ExpressionEngine, PathExpressionEngine, PathExpressionParser}
+import com.atomist.tree.pathexpression.PathExpressionParser
 import com.atomist.tree.utils.TreeNodeUtils
-import org.scalatest.{FlatSpec, Matchers}
+import com.atomist.tree.{MutableTreeNode, TreeNode}
 
-class ScalaFileTypeTest extends FlatSpec with Matchers {
+class ScalaFileTypeTest extends AbstractTypeUnderFileTest {
 
   import PathExpressionParser._
   import ScalaFileTypeTest._
 
-  val ee: ExpressionEngine = new PathExpressionEngine
-
-  val scalaType = new ScalaFileType
+  override val typeBeingTested = new ScalaFileType
 
   it should "ignore ill-formed file without error" in {
-    val scalas = scalaType.findAllIn(ProjectWithBogusScala)
+    val scalas = typeBeingTested.findAllIn(projectWithBogusScala)
     // Should have silently ignored the bogus file
     scalas.size should be(1)
   }
 
   it should "parse hello world" in {
-    val scalas = scalaType.findAllIn(helloWorldProject)
+    val scalas = typeBeingTested.findAllIn(helloWorldProject)
     scalas.size should be(1)
   }
 
   it should "parse hello world and write out correctly" in {
-    val parsed = scalaType.fileToRawNode(HelloWorldScala, Some(ConsoleMatchListener)).get
+    val parsed = typeBeingTested.fileToRawNode(HelloWorldScala, Some(ConsoleMatchListener)).get
     val parsedValue = parsed.value
     withClue(s"Unexpected content: [$parsedValue]") {
       parsedValue should equal(HelloWorldScala.content)
@@ -39,7 +37,7 @@ class ScalaFileTypeTest extends FlatSpec with Matchers {
   }
 
   it should "parse hello world into mutable view and write out unchanged" in {
-    val scalas = scalaType.findAllIn(helloWorldProject)
+    val scalas = typeBeingTested.findAllIn(helloWorldProject)
     scalas.size should be(1)
     scalas.head.head match {
       case mtn: MutableContainerMutableView =>
@@ -50,17 +48,17 @@ class ScalaFileTypeTest extends FlatSpec with Matchers {
 
   it should "find hello world using path expression" in {
     val expr = "//ScalaFile()"
-    val rtn = ee.evaluate(helloWorldProject, expr, DefaultTypeRegistry)
+    val rtn = expressionEngine.evaluate(helloWorldProject, expr, DefaultTypeRegistry)
     rtn.right.get.size should be(1)
   }
 
   it should "find specific exception catch" in {
-    val scalas: Option[Seq[TreeNode]] = scalaType.findAllIn(exceptionsProject)
+    val scalas: Option[Seq[TreeNode]] = typeBeingTested.findAllIn(exceptionsProject)
     scalas.size should be(1)
     val scalaFileNode = scalas.get.head.asInstanceOf[MutableContainerMutableView]
 
     val expr = "//termTryWithCases/case//typeName[@value='ThePlaneHasFlownIntoTheMountain']"
-      ee.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
+    expressionEngine.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
         case Right(nodes) if nodes.nonEmpty =>
           nodes.size should be(1)
           nodes.head.value should be("ThePlaneHasFlownIntoTheMountain")
@@ -72,14 +70,14 @@ class ScalaFileTypeTest extends FlatSpec with Matchers {
 
   it should "find and modify specific exception catch" in {
     val proj = exceptionsProject
-    val scalas: Option[Seq[TreeNode]] = scalaType.findAllIn(proj)
+    val scalas: Option[Seq[TreeNode]] = typeBeingTested.findAllIn(proj)
     scalas.size should be(1)
     val scalaFileNode = scalas.get.head.asInstanceOf[MutableContainerMutableView]
 
     val newException = "MicturationException"
 
     val expr = "//termTryWithCases/case//typeName[@value='ThePlaneHasFlownIntoTheMountain']"
-    ee.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
+    expressionEngine.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
       case Right(nodes) if nodes.nonEmpty =>
         nodes.size should be(1)
         val mut = nodes.head.asInstanceOf[MutableContainerMutableView]
@@ -99,14 +97,14 @@ class ScalaFileTypeTest extends FlatSpec with Matchers {
 
   it should "find and modify multiple points" in {
     val proj = exceptionsProject
-    val scalas: Option[Seq[TreeNode]] = scalaType.findAllIn(proj)
+    val scalas: Option[Seq[TreeNode]] = typeBeingTested.findAllIn(proj)
     scalas.size should be(1)
     val scalaFileNode = scalas.get.head.asInstanceOf[MutableContainerMutableView]
 
     val newException = "MicturationException"
 
     val expr = "//case//typeName[@value='ThePlaneHasFlownIntoTheMountain']"
-    ee.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
+    expressionEngine.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
       case Right(nodes) if nodes.nonEmpty =>
         nodes.size should be(2)
         nodes.foreach {
@@ -128,7 +126,7 @@ class ScalaFileTypeTest extends FlatSpec with Matchers {
 
   it should "find and modify specific exception catch body" in {
     val proj = exceptionsProject
-    val scalas: Option[Seq[TreeNode]] = scalaType.findAllIn(proj)
+    val scalas: Option[Seq[TreeNode]] = typeBeingTested.findAllIn(proj)
     scalas.size should be(1)
     val scalaFileNode = scalas.get.head.asInstanceOf[MutableContainerMutableView]
 
@@ -136,7 +134,7 @@ class ScalaFileTypeTest extends FlatSpec with Matchers {
     //println(TreeNodeUtils.toShorterString(scalaFileNode))
 
     val expr = "//termTryWithCases/case[//typeName[@value='ThePlaneHasFlownIntoTheMountain']]"
-    ee.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
+    expressionEngine.evaluate(scalaFileNode, expr, DefaultTypeRegistry) match {
       case Right(nodes) if nodes.nonEmpty =>
         nodes.size should be(1)
         val mut = nodes.head.asInstanceOf[MutableContainerMutableView]
@@ -162,7 +160,7 @@ object ScalaFileTypeTest {
 
   val BogusScala = StringFileArtifact("Test.scala", "What in God's holy name are you blathering about?")
 
-  val ProjectWithBogusScala =
+  def projectWithBogusScala =
     new ProjectMutableView(EmptyArtifactSource(), SimpleFileBasedArtifactSource(BogusScala))
 
   val HelloWorldScala = StringFileArtifact("Hello.scala",

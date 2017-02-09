02e28d8b9f9bb74f37a7f8472226553c20d2d0c3
Alan Stewart <alankstewart@gmail.com>
2017-02-09 17:25:01 +1100

    Improve inheritance for generated TS interfaces

--- src/main/scala/com/atomist/rug/ts/TypeScriptInterfaceGenerator.scala
+++ src/main/scala/com/atomist/rug/ts/TypeScriptInterfaceGenerator.scala
@@ -11,14 +11,18 @@ import com.atomist.project.{ProjectOperationArguments, SimpleProjectOperationArg
 import com.atomist.rug.kind.DefaultTypeRegistry
 import com.atomist.rug.spi._
 import com.atomist.source.{ArtifactSource, FileArtifact, SimpleFileBasedArtifactSource, StringFileArtifact}
+import com.atomist.tree.TreeNode
 import com.atomist.util.Utils
 import com.atomist.util.lang.TypeScriptGenerationHelper
+import org.apache.commons.lang3.ClassUtils
 
+import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
 
 object TypeScriptInterfaceGenerator extends App {
 
-  val target = if (args.length < 1) "target/classes/@atomist/rug/model/Core.ts" else args.head
+  val target = if (args.length < 1) "target/Core.ts" else args.head
+
   val generator = new TypeScriptInterfaceGenerator
 
   val output = generator.generate("", SimpleProjectOperationArguments("", Map(generator.OutputPathParam -> "Core.ts")))
@@ -27,15 +31,15 @@ object TypeScriptInterfaceGenerator extends App {
 }
 
 /**
-  * Generate types for documents
+  * Generate types for documents.
   *
-  * @param typeRegistry Registry of known Rug Types.
+  * @param typeRegistry registry of known Rug Types.
   */
-class TypeScriptInterfaceGenerator(
-                                    typeRegistry: TypeRegistry = DefaultTypeRegistry,
-                                    config: InterfaceGenerationConfig = InterfaceGenerationConfig()
-                                  ) extends ProjectGenerator
-  with ProjectEditor with ProjectOperationParameterSupport {
+class TypeScriptInterfaceGenerator(typeRegistry: TypeRegistry = DefaultTypeRegistry,
+                                   config: InterfaceGenerationConfig = InterfaceGenerationConfig())
+  extends ProjectGenerator
+    with ProjectEditor
+    with ProjectOperationParameterSupport {
 
   val DefaultTemplateName = "ts.vm"
 
@@ -43,13 +47,15 @@ class TypeScriptInterfaceGenerator(
 
   val OutputPathParam = "output_path"
 
-  val ParentInterface = "ParentType"
+  private val helper = new TypeScriptGenerationHelper()
 
-  val helper = new TypeScriptGenerationHelper()
+  private val root = "TreeNode"
+
+  private val typeSort: (Typed, Typed) => Boolean = (a, b) => a.name <= b.name
 
   private val indent = "    "
 
-  private case class InterfaceType(name: String, description: String, methods: Seq[MethodInfo], parent: String = "TreeNode") {
+  private case class InterfaceType(name: String, description: String, methods: Seq[MethodInfo], parent: String = root) {
 
     override def toString: String = {
       val output = new StringBuilder("")
@@ -94,10 +100,10 @@ class TypeScriptInterfaceGenerator(
     def apply(name: String, paramType: String) = new MethodParam(name, paramType)
   }
 
-  addParameter(Parameter(OutputPathParam, ".*").
-    setRequired(false).
-    setDisplayName("Path for created doc").
-    setDefaultValue(DefaultFilename))
+  addParameter(Parameter(OutputPathParam, ".*")
+    .setRequired(false)
+    .setDisplayName("Path for created doc")
+    .setDefaultValue(DefaultFilename))
 
   @throws[InvalidParametersException](classOf[InvalidParametersException])
   override def generate(projectName: String, poa: ProjectOperationArguments): ArtifactSource = {
@@ -116,10 +122,62 @@ class TypeScriptInterfaceGenerator(
        | */""".stripMargin
   }
 
-  val typeSort: (Typed, Typed) => Boolean = (a, b) => a.name <= b.name
+  private def allInterfaceTypes(allTypes: Seq[Typed]): Seq[InterfaceType] = {
+    val interfaceTypes = new ListBuffer[InterfaceType]
 
-  private def allInterfaceTypes(allTypes: Seq[Typed]): Seq[InterfaceType] =
-    allTypes.map(t => InterfaceType(t.name, t.description, allMethods(t)))
+    allTypes.foreach(t => {
+      val operations = (t.typeInformation match {
+        case st: StaticTypeInformation => st.operations
+        case _ => null
+      }).filterNot(_ == null)
+
+      operations.foreach(op => {
+        val superclasses = ClassUtils.hierarchy(op.definedOn, ClassUtils.Interfaces.INCLUDE).asScala
+          .filterNot(c => allMethods(ReflectiveFunctionExport.exportedOperations(c)).isEmpty)
+          .filterNot(c => classOf[TreeNode] == c) // Ignore TreeNode as it's already present in TS file
+          .drop(1) // Drop leaf class
+          .toList
+
+        // Add superclasses
+        for (i <- 0 until superclasses.size) {
+          val ops = ReflectiveFunctionExport.exportedOperations(superclasses(i))
+          val parent = if (i == superclasses.size - 1) root else Typed.typeToTypeName(superclasses(i + 1))
+          val name = Typed.typeToTypeName(superclasses(i))
+          interfaceTypes += InterfaceType(name, name, allMethods(ops), parent)
+        }
+
+        // Add leaf class
+        val parent = if (superclasses.isEmpty) root else Typed.typeToTypeName(superclasses(0))
+        interfaceTypes += InterfaceType(t.name, t.description, allMethods(t), parent)
+      })
+    })
+
+    (interfaceTypes.groupBy(it => it.name) map {
+      case (_, l) => l.head
+    }).toSeq
+  }
+
+  private def allMethods(t: Typed): Seq[MethodInfo] =
+    t.typeInformation match {
+      case st: StaticTypeInformation => allMethods(st.operations)
+      case _ => Nil
+    }
+
+  private def allMethods(ops: Seq[TypeOperation]): Seq[MethodInfo] = {
+    val methods = new ListBuffer[MethodInfo]
+    for {
+      op <- ops
+      if shouldEmit(op)
+    } {
+      val params =
+        for (p <- op.parameters)
+          yield
+            MethodParam(p.name, helper.javaTypeToTypeScriptType(p.parameterType))
+
+      methods += MethodInfo(op.name, params, helper.javaTypeToTypeScriptType(op.returnType))
+    }
+    methods
+  }
 
   private def emitInterfaces(poa: ProjectOperationArguments): FileArtifact = {
     val alreadyGenerated = ListBuffer.empty[InterfaceType]
@@ -130,9 +188,6 @@ class TypeScriptInterfaceGenerator(
     output ++= config.separator
 
     val allTypes = typeRegistry.types.sortWith(typeSort)
-    val tys = allTypes.toList
-    // TODO process unpublishedTypes
-    val unpublishedTypes = findUnpublishedTypes(allTypes)
     val interfaceTypes = allInterfaceTypes(allTypes)
     for {
       t <- interfaceTypes
@@ -151,36 +206,6 @@ class TypeScriptInterfaceGenerator(
     StringFileArtifact(
       poa.stringParamValue(OutputPathParam),
       output.toString())
-  }
-
-  private def allMethods(t: Typed): Seq[MethodInfo] = {
-    val methods = new ListBuffer[MethodInfo]
-    t.typeInformation match {
-      case st: StaticTypeInformation =>
-        for {
-          op <- st.operations
-          if shouldEmit(op)
-        } {
-          val params =
-            for (p <- op.parameters)
-              yield
-                MethodParam(p.name, helper.javaTypeToTypeScriptType(p.parameterType))
-
-          methods += MethodInfo(op.name, params, helper.javaTypeToTypeScriptType(op.returnType))
-        }
-      case _ =>
-    }
-    methods.toList
-  }
-
-  // Find all the types that aren't published types but define methods
-  private def findUnpublishedTypes(publishedTypes: Seq[Typed]): Seq[String] = {
-    val allOperations = publishedTypes.map(_.typeInformation).collect {
-      case st: StaticTypeInformation => st.operations
-    }.flatten
-    val types = allOperations.map(op => Typed.typeToTypeName(op.definedOn)).toSet
-    val publishedTypeNames = publishedTypes.map(_.name).toSet
-    (types -- publishedTypeNames).toSeq.sorted
   }
 
   override def modify(as: ArtifactSource, poa: ProjectOperationArguments): ModificationAttempt = {
@@ -211,7 +236,7 @@ case class InterfaceGenerationConfig(
   val licenseHeader: String =
     """
       |/*
-      | * Copyright 2015-2016 Atomist Inc.
+      | * Copyright 2015-2017 Atomist Inc.
       | *
       | * Licensed under the Apache License, Version 2.0 (the "License");
       | * you may not use this file except in compliance with the License.

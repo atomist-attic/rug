1c5af9f180bfc7b9365ce63e9703465e98cd6c2e
Rod Johnson <rod@atomist.com>
2017-02-16 17:57:00 +1100

    More YmlFileTypeTests

--- src/test/scala/com/atomist/rug/kind/yml/AbstractYmlUsageTest.scala
+++ src/test/scala/com/atomist/rug/kind/yml/AbstractYmlUsageTest.scala
@@ -1,5 +1,7 @@
 package com.atomist.rug.kind.yml
 
+import java.io.StringReader
+
 import com.atomist.project.SimpleProjectOperationArguments
 import com.atomist.project.edit.{NoModificationNeeded, SuccessfulModification}
 import com.atomist.rug.DefaultRugPipeline
@@ -7,9 +9,12 @@ import com.atomist.rug.InterpreterRugPipeline.DefaultRugArchive
 import com.atomist.rug.TestUtils.attemptModification
 import com.atomist.source._
 import org.scalatest.{FlatSpec, Matchers}
+import org.yaml.snakeyaml.Yaml
+import scala.collection.JavaConverters._
 
-// TODO this isn't YML specific so could be pulled into generic infrastructure
 abstract class AbstractYmlUsageTest extends FlatSpec with Matchers {
+
+  private val parser = new Yaml()
 
   protected def runProgAndCheck(prog: String, as: ArtifactSource, mods: Int): ArtifactSource = {
     val progArtifact: ArtifactSource = new SimpleFileBasedArtifactSource(DefaultRugArchive,
@@ -22,6 +27,14 @@ abstract class AbstractYmlUsageTest extends FlatSpec with Matchers {
     modAttempt match {
       case sm: SuccessfulModification =>
         assert(sm.result.cachedDeltas.size === mods)
+        sm.result.cachedDeltas.foreach {
+          case fud: FileUpdateDelta =>
+//            val yml = fud.updatedFile.content
+//            val events = parser.parse(new StringReader(yml))
+//            println(s"Parsed ${fud.updatedFile}: ${events.asScala.mkString(",")}")
+            // TODO how do we validate YML? SnakeYAML seems to let everything through
+          case x => fail(s"Unexpected change: $x")
+        }
         sm.result
       case _: NoModificationNeeded if mods > 0 =>
         fail(s"No modification made when $mods expected: $prog; \n${ArtifactSourceUtils.prettyListFiles(as)}")
--- src/test/scala/com/atomist/rug/kind/yml/path/YmlFileTypeUsageTest.scala
+++ src/test/scala/com/atomist/rug/kind/yml/path/YmlFileTypeUsageTest.scala
@@ -1,33 +1,58 @@
 package com.atomist.rug.kind.yml.path
 
-import com.atomist.rug.kind.yml.AbstractYmlUsageTest
+import com.atomist.rug.kind.yml.{AbstractYmlUsageTest, YmlUsageTestTargets}
 import com.atomist.rug.kind.yml.YmlUsageTestTargets.allAS
 
 class YmlFileTypeUsageTest extends AbstractYmlUsageTest {
 
-//  it should "get group value with no change with native Rug function" in {
-//    val prog =
-//      """
-//        |editor YmlEdit
-//        |
-//        |with File f when path = "x.yml"
-//        | with YmlFile y
-//        |   do valueOf "group"
-//      """.stripMargin
-//    allAS.foreach(asChanges => runProgAndCheck(prog, asChanges._1, 0))
-//  }
-
-  it should "change group value with native Rug function" in pendingUntilFixed {
+  it should "change scalar value value" in {
+    val newContent = "Marx Brothers"
     val prog =
-      """
+      s"""
         |editor YmlEdit
         |
-        |with File f when path = "x.yml"
-        | with YmlFile x
-        |   with group
-        |     do update "Marx Brothers"
+        |let group = $$(/*[@name='x.yml']/YmlFile()/group)
+        |
+        |with group
+        |     do update "$newContent"
       """.stripMargin
-    allAS.foreach(asChanges => runProgAndCheck(prog, asChanges._1, 1))
+    allAS.foreach(asChanges => {
+      val r = runProgAndCheck(prog, asChanges._1, 1)
+      assert(r.findFile("x.yml").get.content == YmlUsageTestTargets.xYml.replace("queen", newContent))
+    })
+  }
+
+  it should "change scalar value late in document" in {
+    val newContent = "earth"
+    val prog =
+      s"""
+         |editor YmlEdit
+         |
+         |let group = $$(/*[@name='x.yml']/YmlFile()/common)
+         |
+         |with group
+         |     do update "$newContent"
+      """.stripMargin
+    allAS.foreach(asChanges => {
+      val r = runProgAndCheck(prog, asChanges._1, 1)
+      assert(r.findFile("x.yml").get.content == YmlUsageTestTargets.xYml.replace("everywhere", newContent))
+    })
+  }
+
+  it should "change collection elements" in {
+    val prog =
+      s"""
+         |editor YmlEdit
+         |
+         |let group = $$(/*[@name='x.yml']/YmlFile()/dependencies/*)
+         |
+         |with group g
+         |     do update { g.value().replace("Death", "Life") } # Capitals are only present in the dependencies
+      """.stripMargin
+    allAS.foreach(asChanges => {
+      val r = runProgAndCheck(prog, asChanges._1, 1)
+      assert(r.findFile("x.yml").get.content == YmlUsageTestTargets.xYml.replace("Death", "Life"))
+    })
   }
 
 }

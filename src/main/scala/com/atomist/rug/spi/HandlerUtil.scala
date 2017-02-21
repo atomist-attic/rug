79d570f1395a39966b6bbbdeb6b6d7f632cb9154
Jessica Kerr <jessitron@gmail.com>
2017-02-21 11:15:52 -0600

    Handler tree printing utils

---
+++ src/main/scala/com/atomist/rug/spi/HandlerUtil.scala
@@ -0,0 +1,88 @@
+package com.atomist.rug.spi
+
+import Handlers._
+import Instruction._
+import com.atomist.rug.tree.utils.TreeNodePrinter
+import com.atomist.rug.tree.utils.TreeNodePrinter.BabyTree
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+object HandlerUtil {
+
+  import com.atomist.rug.spi.Handlers._
+
+  private def instructionToString(i: Instruction) = {
+    val op = i match {
+      case Generate(detail) => "Generate"
+      case Edit(detail) => "Edit"
+      case Review(detail) => "Review"
+      case Execute(detail) => "Execute"
+      case Command(detail) => "Command"
+      case Respond(detail) => "Respond"
+    }
+    s"${op} ${i.detail.name} ${i.detail.coordinates} ${i.detail.parameters}"
+  }
+
+  private def messageToTree(m: Message) = {
+    val messageString = s"Message: ${m.body}${m.channelId.map(" to " + _).getOrElse("")}, like ${m.instructions}"
+    BabyTree(messageString)
+  }
+  private def callbackToTree(name: String, c: Callback): BabyTree = BabyTree(name, Seq(c match {
+    case p: Plan => planToTree(p)
+    case m: Message => messageToTree(m)
+    case r: Respond => BabyTree(instructionToString(r))
+  }))
+
+  def planToTree(plan: Plan): BabyTree = {
+
+
+
+    def respondableToTree(respondable: Respondable): BabyTree = {
+      val i = instructionToString(respondable.instruction)
+
+      val onSuccessChild = respondable.onSuccess.map(callbackToTree("onSuccess", _))
+      val onFailureChild = respondable.onFailure.map(callbackToTree("onFailure", _))
+
+      BabyTree(i, onSuccessChild.toSeq ++ onFailureChild)
+    }
+
+    BabyTree("Plan",
+      plan.instructions.sortBy(_.toString).map(respondableToTree) ++
+        plan.messages.sortBy(_.toString).map(messageToTree))
+  }
+
+  def printPlan(plan: Plan): String = {
+    val tree: BabyTree = planToTree(plan)
+    TreeNodePrinter.draw[BabyTree](_.children, _.print)(tree)
+  }
+
+  private def awaitAndTreeLogEvents(name: String, events: Seq[PlanLogEvent]): BabyTree = {
+    def logEventToTree(one: PlanLogEvent): BabyTree = {
+      def nullSafeMessage(x: Throwable) = s"Error: ${Option(x).map(_.getMessage).getOrElse("null exception")}"
+
+      one match {
+        case InstructionError(i, x) => BabyTree(nullSafeMessage(x), Seq(BabyTree(instructionToString(i))))
+        case MessageDeliveryError(m, x) => BabyTree(nullSafeMessage(x), Seq(messageToTree(m)))
+        case CallbackError(c, x) => BabyTree(nullSafeMessage(x), Seq(callbackToTree("failed callback", c)))
+        case InstructionResponse(instruction, response) =>
+          BabyTree(s"Received $response back from ${instructionToString(instruction)}")
+        case NestedPlanRun(plan, planResult) =>
+          Await.ready(planResult, 10.seconds)
+          val result = planResult.value.get match {
+            case Failure(exception) => BabyTree(s"Future failed with ${exception.getMessage}")
+            case Success(value) => awaitAndTreeLogEvents(s"Future completed", value.log)
+          }
+          val planChild = planToTree(plan)
+          BabyTree("Nested plan run", Seq(planChild, result))
+      }
+    }
+
+    val children = events.map(logEventToTree)
+    BabyTree(name, children.sortBy(_.print))
+  }
+
+  def drawEventLogs(name: String, events: Seq[PlanLogEvent]) =
+    TreeNodePrinter.drawTree(awaitAndTreeLogEvents(name, events))
+}

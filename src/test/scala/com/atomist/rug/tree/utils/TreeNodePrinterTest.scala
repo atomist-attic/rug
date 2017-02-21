a606cccde31c896b88462dc4581fd0daf376a491
Atomist <cli@atomist.com>
2017-02-21 11:15:14 -0600

    This rug is copied into your project by AddRugEditor
    ```
    ---
    kind: "operation"
    client: "rug-cli 0.23.1-SNAPSHOT"
    editor:
      name: "jessitron.microlib-TreeNodePrinter.BringIn"
      group: "jessitron"
      artifact: "microlib-TreeNodePrinter"
      version: "0.1.0"
      origin:
        repo: "n/a"
        branch: "n/a"
        sha: "n/a"
      parameters:
        - "package": "com.atomist.rug.tree.utils"
    
    ```

---
+++ src/test/scala/com/atomist/rug/tree/utils/TreeNodePrinterTest.scala
@@ -0,0 +1,34 @@
+package com.atomist.rug.tree.utils
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class TreeNodePrinterTest extends FlatSpec with Matchers {
+
+  case class TreeNode(print: String, children: Seq[TreeNode] = Seq())
+
+  val drawTree: TreeNode => String = TreeNodePrinter.draw[TreeNode](_.children, _.print)
+
+  it should "conform to the example" in {
+    val expected = """Grandma
+├─┬ Dad
+| ├── Sister
+| └─┬ Me
+|   ├── Daughter
+|   └── Son
+└── Aunt"""
+
+    val input =
+      TreeNode("Grandma",Seq(
+        TreeNode("Dad",Seq(
+            TreeNode("Sister"),
+            TreeNode("Me",Seq(
+              TreeNode("Daughter"),
+              TreeNode("Son"))))),
+        TreeNode("Aunt")))
+
+    val output = drawTree(input)
+
+    output should be(expected)
+
+  }
+}

18b98eec6c212a3a140bdb569a7866bd48e79f1b
Rod Johnson <rod@atomist.com>
2017-02-23 19:43:44 +1100

    Using well-known path expression

--- src/test/resources/com/atomist/rug/kind/java/path/CatchThrowable.ts
+++ src/test/resources/com/atomist/rug/kind/java/path/CatchThrowable.ts
@@ -1,27 +1,39 @@
 import {Project} from '@atomist/rug/model/Core'
 import {ProjectReviewer} from '@atomist/rug/operations/ProjectReviewer'
 import {PathExpression,PathExpressionEngine} from '@atomist/rug/tree/PathExpression'
-import {Parameter,ReviewResult,ReviewComment,Severity} from '@atomist/rug/operations/RugOperation'
+import {ReviewResult,ReviewComment,Severity} from '@atomist/rug/operations/RugOperation'
 import {DecoratingPathExpressionEngine} from '@atomist/rug/ast/DecoratingPathExpressionEngine'
 import {RichTextTreeNode} from '@atomist/rug/ast/TextTreeNodeOps'
+import {Parameter, Reviewer} from '@atomist/rug/operations/Decorators'
+
+import * as java from '@atomist/rug/ast/java/Types'
 
 class CatchThrowable implements ProjectReviewer {
-    name: string = "AddUsing"
-    description: string = "Find "
+
+    name = "CatchThrowable"
+
+    description = "Look for particular throwables"
+
+    @Parameter({description: "Exception to look for", pattern: "^.*$"})
+    exception: string
 
     review(project: Project) {
-      let eng: PathExpressionEngine = 
+      let eng = 
       new DecoratingPathExpressionEngine(project.context().pathExpressionEngine())
 
       let rr = ReviewResult.empty(this.name)
 
-      eng.with<RichTextTreeNode>(project, "//JavaFile()//catchClause//catchType[@value='ThePlaneHasFlownIntoTheMountain']", n => {
+      eng.withExpression<RichTextTreeNode>(project, new java.Catch(this.exception), n => {
         //console.log(`The using was '${n.value()}'`)
       
         rr.add(n.commentConcerning(
                     this.name,
                     Severity.Major)
         )
+       })
+
+       eng.withExpression<RichTextTreeNode>(project, java.CatchException, n => {
+         throw new Error("Shouldn't have caught exception")
        })
 
        return rr
--- src/test/scala/com/atomist/rug/kind/java/path/JavaFileTypeUsageTest.scala
+++ src/test/scala/com/atomist/rug/kind/java/path/JavaFileTypeUsageTest.scala
@@ -3,6 +3,7 @@ package com.atomist.rug.kind.java.path
 import com.atomist.param.SimpleParameterValues
 import com.atomist.rug.TestUtils._
 import com.atomist.rug.kind.grammar.AbstractTypeUnderFileTest
+import com.atomist.tree.content.text.LineInputPositionImpl
 
 class JavaFileTypeUsageTest extends AbstractTypeUnderFileTest {
 
@@ -10,16 +11,21 @@ class JavaFileTypeUsageTest extends AbstractTypeUnderFileTest {
 
   override val typeBeingTested = new JavaFileType
 
-  it should "find catch(throwable)" in {
+  it should "find catch(throwable) and validate format info" in {
+    val exceptionToSearchFor = "ThePlaneHasFlownIntoTheMountain"
 
-    val rr = reviewerInSideFile(this, "CatchThrowable.ts").review(ExceptionsSources, SimpleParameterValues.Empty)
+    val rr = reviewerInSideFile(this, "CatchThrowable.ts").review(ExceptionsSources,
+      SimpleParameterValues(Map("exception" -> exceptionToSearchFor)))
     rr.comments.nonEmpty should be (true)
     assert(rr.comments.size === 1)
     val c1 = rr.comments.head
     c1.line should be (defined)
     c1.column should be (defined)
     c1.fileName.get should be (Exceptions.path)
-    //println(c1)
+
+    val pos = LineInputPositionImpl(Exceptions.content, c1.line.get, c1.column.get)
+    val extracted = Exceptions.content.substring(pos.offset, pos.offset + exceptionToSearchFor.length)
+    assert(extracted === exceptionToSearchFor)
   }
 
 }

0eeeba4768e382b8ae6be1152a896d9b1e8146e8
Christian Dupuis <cd@atomist.com>
2017-02-24 22:36:22 +0100

    Cache serviceloader results keyed by ClassLoader
    In a shared classloader environment the previous approach using lazy, only loaded Types and RugFunctions for the first archvie/classpath. Now the cache is keyed by ClassLoader with weak reference so the ClassLoader can get collected if possible.

--- src/main/scala/com/atomist/rug/kind/ServiceLoaderTypeRegistry.scala
+++ src/main/scala/com/atomist/rug/kind/ServiceLoaderTypeRegistry.scala
@@ -1,11 +1,11 @@
 package com.atomist.rug.kind
 
-import _root_.java.util.ServiceLoader
-
 import com.atomist.rug.RugRuntimeException
 import com.atomist.rug.spi._
 import com.typesafe.scalalogging.LazyLogging
+import _root_.java.util.ServiceLoader
 
+import _root_.scala.collection.mutable.WeakHashMap
 import _root_.scala.collection.JavaConverters._
 
 /**
@@ -19,17 +19,27 @@ class ServiceLoaderTypeRegistry
   extends TypeRegistry
     with LazyLogging {
 
-  private lazy val typesMap: Map[String, Typed] = {
-    ServiceLoader.load(classOf[Typed]).asScala.map {
-      case t: Typed =>
-        logger.info(s"Registered type extension '${t.name}, with class ${t.getClass},description=${t.description}")
-        val st = t
-        logger.debug(s"Found operations: ${st.allOperations.map(_.name).mkString(",")}")
-        t.name -> t
-      case wtf =>
-        throw new RugRuntimeException("ExtensionType", s"Type class ${wtf.getClass} must implement Typed interface", null)
+  // Sharing Rug in a hierarchy of CLassLoaders requires the ServiceLoader to be triggered
+  // for each ClassLoader; using WeakHashMap so ClassLoader references can be garbage collected
+  private val typesMapByClassLoader = WeakHashMap[ClassLoader, Map[String, Typed]]()
+
+  private def typesMap: Map[String, Typed] = {
+    typesMapByClassLoader.get(Thread.currentThread().getContextClassLoader) match {
+      case Some(tm) => tm
+      case _ =>
+        val typesMap: Map[String, Typed] = ServiceLoader.load(classOf[Typed]).asScala.map {
+          case t: Typed =>
+            logger.info(s"Registered type extension '${t.name}, with class ${t.getClass},description=${t.description}")
+            val st = t
+            logger.debug(s"Found operations: ${st.allOperations.map(_.name).mkString(",")}")
+            t.name -> t
+          case wtf =>
+            throw new RugRuntimeException("ExtensionType", s"Type class ${wtf.getClass} must implement Typed interface", null)
+        }.toMap
+        typesMapByClassLoader += Thread.currentThread().getContextClassLoader -> typesMap
+        typesMap
     }
-  }.toMap
+  }
 
   override def findByName(kind: String): Option[Typed] = typesMap.get(kind)
 
--- src/main/scala/com/atomist/rug/runtime/plans/ServiceLoaderRugFunctionRegistry.scala
+++ src/main/scala/com/atomist/rug/runtime/plans/ServiceLoaderRugFunctionRegistry.scala
@@ -2,28 +2,41 @@ package com.atomist.rug.runtime.plans
 
 import java.util.ServiceLoader
 
-import com.atomist.rug.spi.{RugFunction, RugFunctionRegistry}
+import com.atomist.rug.RugRuntimeException
+import com.atomist.rug.spi.{RugFunction, RugFunctionRegistry, Typed}
 import com.typesafe.scalalogging.LazyLogging
 
+import _root_.scala.collection.mutable.WeakHashMap
 import scala.collection.JavaConverters._
 
 /**
   * Use ServiceLoader to load RugFunction in to a registry
   */
 class ServiceLoaderRugFunctionRegistry
-  extends RugFunctionRegistry with LazyLogging {
+  extends RugFunctionRegistry
+    with LazyLogging {
 
-  private lazy val functions: Map[String, RugFunction] = {
-    try{
-      ServiceLoader.load(classOf[RugFunction]).asScala.map(r => (r.name, r)).toMap
-    }catch {
-      case x: Throwable =>
-        x.printStackTrace()
-        Map()
+  // Sharing Rug in a hierarchy of CLassLoaders requires the ServiceLoader to be triggered
+  // for each ClassLoader; using WeakHashMap so ClassLoader references can be garbage collected
+  private val functionsByClassLoader = WeakHashMap[ClassLoader, Map[String, RugFunction]]()
+
+  private def functionsMap: Map[String, RugFunction] = {
+    functionsByClassLoader.get(Thread.currentThread().getContextClassLoader) match {
+      case Some(tm) => tm
+      case _ =>
+        val functionssMap: Map[String, RugFunction] = ServiceLoader.load(classOf[RugFunction]).asScala.map {
+          case t: Typed =>
+            logger.info(s"Registered rug function '${t.name}, with class ${t.getClass},description=${t.description}")
+            t.name -> t
+          case wtf =>
+            throw new RugRuntimeException("RugFunction", s"RugFunction class ${wtf.getClass} must implement RugFunction interface", null)
+        }.toMap
+        functionsByClassLoader += Thread.currentThread().getContextClassLoader -> functionssMap
+        functionssMap
     }
   }
 
   override def find(name: String): Option[RugFunction] = {
-    functions.get(name)
+    functionsMap.get(name)
   }
 }

9b0bc6ae5573654270648c042d050597d0d66816
Jessica Kerr <jessitron@gmail.com>
2017-02-28 23:41:51 -0600

    Begin conversion of JavaTypeUsageTest into typescript
    I wrote an editor (included here) to pull the triple-quoted string Rugs
    into separate files in test/resources.
    Then I ran the ConvertRugToTypeScript editor (from another branch of rug)
    to convert them into typescript.
    and changed the test class to not use the Rug pipeline.
    
    Many of them worked, some of them didn't. I think those are real problems
    and I don't know how to fix those.
    These have been marked pendingUntilFixed { /* broken in TypeScript conversion */

--- src/main/scala/com/atomist/project/archive/RugDslArchiveReader.scala
+++ src/main/scala/com/atomist/project/archive/RugDslArchiveReader.scala
@@ -1,17 +1,6 @@
 package com.atomist.project.archive
 
-import com.atomist.project.ProjectOperation
-import com.atomist.project.generate.{EditorInvokingProjectGenerator, ProjectGenerator}
-import com.atomist.project.review.ProjectReviewer
-import com.atomist.rug.kind.DefaultTypeRegistry
-import com.atomist.rug.runtime.AddressableRug
-import com.atomist.rug.runtime.rugdsl.{DefaultEvaluator, Evaluator, RugDrivenProjectEditor}
-import com.atomist.rug.spi.TypeRegistry
-import com.atomist.rug.{DefaultRugPipeline, EmptyRugDslFunctionRegistry, Import}
 import com.atomist.source.ArtifactSource
-import com.typesafe.scalalogging.LazyLogging
-
-import scala.collection.Seq
 
 
 

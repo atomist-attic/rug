83b9b26e0fb1fd563f85137a4f142d63add6fa2d
Christian Dupuis <cd@atomist.com>
2017-03-07 17:45:57 +0100

    Support for calling nested editors from generators

--- src/test/resources/com/atomist/rug/test/gherkin/SimpleGeneratorWithParams.ts
+++ src/test/resources/com/atomist/rug/test/gherkin/SimpleGeneratorWithParams.ts
@@ -2,7 +2,7 @@ import {Project} from '@atomist/rug/model/Core'
 import {Generator} from '@atomist/rug/operations/Decorators'
 import {Parameter} from '@atomist/rug/operations/Decorators'
 
-@Generator("SimpleGenerator","My simple Generator")
+@Generator("SimpleGeneratorWithParams","My simple Generator")
 export class SimpleGeneratorWithParams {
 
     @Parameter({description: "text", pattern: "^[a-zA-Z ]*$"})

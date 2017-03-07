2098a1b9898ab0c110e3bfc3faf6323e6be886a1
Clay McCoy <clay@atomist.com>
2017-03-06 20:47:46 -0600

    Move long running test to somewhere related to the actual class under test.

--- src/test/scala/com/atomist/rug/runtime/plans/LocalPlanRunnerTest.scala
+++ src/test/scala/com/atomist/rug/runtime/plans/LocalPlanRunnerTest.scala
@@ -231,44 +231,4 @@ class LocalPlanRunnerTest extends FunSpec with Matchers with OneInstancePerTest 
 
     verifyNoMoreInteractions(messageDeliverer, logger)
   }
-  val simpleCommandWithObjectInstructionParamAsJson =
-    StringFileArtifact(DefaultAtomistConfig.handlersRoot + "/Handler.ts",
-      """
-        |import {HandleCommand, HandleResponse, Message, Instruction, Response, HandlerContext, Plan} from '@atomist/rug/operations/Handlers'
-        |import {CommandHandler, ResponseHandler, Parameter, Tags, Intent} from '@atomist/rug/operations/Decorators'
-        |
-        |@CommandHandler("ShowMeTheKitties","Search Youtube for kitty videos and post results to slack")
-        |@Tags("kitty", "youtube", "slack")
-        |@Intent("show me kitties","cats please")
-        |class KittieFetcher implements HandleCommand{
-        |
-        |  handle(ctx: HandlerContext) : Plan {
-        |
-        |    let result = new Plan()
-        |    result.add({instruction: {kind: "execute", name: "ExampleFunction", parameters: {jsonparam: {mucho: "coolness"}}},
-        |                onSuccess: {name: "SimpleResponseHandler", kind: "respond"} });
-        |    return result;
-        |  }
-        |}
-        |
-        |@ResponseHandler("SimpleResponseHandler", "Checks response is equal to passed in parameter")
-        |class Responder implements HandleResponse<String> {
-        |  handle(response: Response<string>) : Plan {
-        |    return new Plan();
-        |  }
-        |}
-        |
-        |export let respond = new Responder();
-        |
-        |export let command = new KittieFetcher();
-        |
-    """.stripMargin)
-
-  it ("should serialize complex instruction parameters to json during plan building") {
-    val rugArchive = TypeScriptBuilder.compileWithModel(SimpleFileBasedArtifactSource(simpleCommandWithObjectInstructionParamAsJson))
-    val rugs = RugArchiveReader.find(rugArchive, Nil)
-    val com = rugs.commandHandlers.head
-    val plan = com.handle(null,SimpleParameterValues.Empty).get
-    assert(plan.instructions.head.instruction.detail.parameters.head.getValue === """{"mucho":"coolness"}""")
-  }
 }

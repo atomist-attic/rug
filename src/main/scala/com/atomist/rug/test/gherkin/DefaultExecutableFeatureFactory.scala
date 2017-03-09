0ada2210379c451aba5009b39f8730cabfdbc517
Rod Johnson <rod@atomist.com>
2017-03-10 08:56:00 +1100

    Removed immature handler code

--- src/main/scala/com/atomist/rug/test/gherkin/DefaultExecutableFeatureFactory.scala
+++ src/main/scala/com/atomist/rug/test/gherkin/DefaultExecutableFeatureFactory.scala
@@ -1,13 +1,12 @@
 package com.atomist.rug.test.gherkin
 
 import com.atomist.project.archive.{DefaultAtomistConfig, Rugs}
-import com.atomist.rug.test.gherkin.handler.HandlerFeature
 import com.atomist.rug.test.gherkin.project.ProjectManipulationFeature
 import com.atomist.source.ArtifactSource
 
 /**
   * Default implementation of ExecutableFeatureFactory, which
-  * knows about project features and handler features, which it identifies
+  * knows about project features and handler features (coming soon), which it identifies
   * by their respective subdirectories under .atomist/test
   */
 object DefaultExecutableFeatureFactory extends ExecutableFeatureFactory {
@@ -17,8 +16,6 @@ object DefaultExecutableFeatureFactory extends ExecutableFeatureFactory {
   override def executableFeatureFor(f: FeatureDefinition, definitions: Definitions, rugAs: ArtifactSource, rugs: Option[Rugs]): AbstractExecutableFeature[_,_] = {
     if (f.definition.path.contains("project"))
       new ProjectManipulationFeature(f, definitions, rugAs, rugs)
-    else if (f.definition.path.contains("handler"))
-      new HandlerFeature(f, definitions, rugAs, rugs)
     else {
       throw new IllegalArgumentException(s"Cannot handle path [${f.definition.path}]: Paths must be of form [${atomistConfig.testsDirectory}/project] or ${atomistConfig.testsDirectory}/handlers]")
     }

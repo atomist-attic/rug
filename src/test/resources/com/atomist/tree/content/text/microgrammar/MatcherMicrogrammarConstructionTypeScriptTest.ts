df5360157418ef742d4a8341b5ee873a6c1f1490
Jessica Kerr <jessitron@gmail.com>
2017-03-10 18:48:09 -0600

    Implement 'or' matcher constructor

--- src/test/resources/com/atomist/tree/content/text/microgrammar/MatcherMicrogrammarConstructionTypeScriptTest.ts
+++ src/test/resources/com/atomist/tree/content/text/microgrammar/MatcherMicrogrammarConstructionTypeScriptTest.ts
@@ -1,14 +1,23 @@
 import { Project } from '@atomist/rug/model/Core'
 import { Editor } from '@atomist/rug/operations/Decorators'
+import { Microgrammar, TextTreeNode } from '@atomist/rug/tree/PathExpression'
+import { Or } from '@atomist/rug/tree/Microgrammars'
+
 
 @Editor("MatcherMicrogrammarConstructionTypeScriptTest", "Uses MatcherMicrogrammarConstruction from TypeScript")
 class MatcherMicrogrammarConstructionTypeScriptTest {
 
     edit(project: Project) {
 
-        let pom = project.findFile('pom.xml');
+        let mg = new Microgrammar("testMe", `I like $vegetable`,
+            { vegetable: Or(["broccoli", "carrots"]) });
 
-        pom.replace('dependency', 'dependenciesAreForBirds');
+        let eng = project.context().pathExpressionEngine().addType(mg);
+
+        eng.with<TextTreeNode>(project, "/targetFile/testMe()", e => {
+            //console.log("Found " + e.value())
+            e.update(e.value() + " (which is a vegetable)")
+        })
 
     }
 }

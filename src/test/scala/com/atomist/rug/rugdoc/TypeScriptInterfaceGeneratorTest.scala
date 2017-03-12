1e66207e03471fd8d2bb50f10bc32e11ce038d8b
Rod Johnson <rod@atomist.com>
2017-03-12 11:35:03 +1100

    Emitted interfaces are compiling

--- src/test/scala/com/atomist/rug/rugdoc/TypeScriptInterfaceGeneratorTest.scala
+++ src/test/scala/com/atomist/rug/rugdoc/TypeScriptInterfaceGeneratorTest.scala
@@ -2,12 +2,40 @@ package com.atomist.rug.rugdoc
 
 import com.atomist.param.SimpleParameterValues
 import com.atomist.rug.ts.{InterfaceGenerationConfig, TypeScriptBuilder, TypeScriptInterfaceGenerator}
-import com.atomist.source.{FileArtifact, FileEditor}
+import com.atomist.source.{ArtifactSource, FileArtifact, FileEditor}
 import org.scalatest.{FlatSpec, Matchers}
 
-class TypeScriptInterfaceGeneratorTest extends FlatSpec with Matchers {
+object TypeScriptInterfaceGeneratorTest {
 
   val tsc = TypeScriptBuilder.compiler
+
+  /**
+    * We need to get rid of the imports as they'll fail
+    * when we try to compile the file on its own
+    */
+  private def withoutImports(output: ArtifactSource): ArtifactSource =
+    output ✎ new FileEditor {
+      override def canAffect(f: FileArtifact): Boolean = true
+
+      // Note: We need to pretend we have imports that will be available
+      // at runtime
+      override def edit(f: FileArtifact): FileArtifact =
+      f.withContent(f.content.replace(InterfaceGenerationConfig().imports,
+        """
+          |interface ProjectContext {}
+          |interface PathExpressionEngine {}
+          |interface TreeNode {}
+          |interface FormatInfo {}
+        """.stripMargin))
+    }
+
+  def compile(output: ArtifactSource): ArtifactSource = {
+    val withoutImport = TypeScriptInterfaceGeneratorTest.withoutImports(output)
+    tsc.compile(withoutImport)
+  }
+}
+
+class TypeScriptInterfaceGeneratorTest extends FlatSpec with Matchers {
 
   it should "generate compilable typescript file" in {
     val td = new TypeScriptInterfaceGenerator()
@@ -16,23 +44,7 @@ class TypeScriptInterfaceGeneratorTest extends FlatSpec with Matchers {
       Map(td.OutputPathParam -> ".atomist/editors/Interfaces.ts")))
     assert(output.allFiles.size > 1)
 
-    // We need to get rid of the imports as they'll fail when we try to compile the file on its own
-    val withoutImport = output ✎ new FileEditor {
-      override def canAffect(f: FileArtifact): Boolean = true
-
-      // Note: We need to pretend we have imports that will be available
-      // at runtime
-      override def edit(f: FileArtifact): FileArtifact =
-        f.withContent(f.content.replace(InterfaceGenerationConfig().imports,
-          """
-            |interface ProjectContext {}
-            |interface PathExpressionEngine {}
-            |interface TreeNode {}
-            |interface FormatInfo {}
-          """.stripMargin))
-    }
-
-    val compiled = tsc.compile(withoutImport)
+    val compiled = TypeScriptInterfaceGeneratorTest.compile(output)
     val ts = compiled.allFiles.find(_.name.endsWith(".ts"))
     ts shouldBe defined
     // println(ts.get.content)

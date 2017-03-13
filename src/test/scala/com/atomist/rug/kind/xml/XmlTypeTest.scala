3eaf94287f47cc4f014b8518ff5c76d8bc57fae2
David Dooling <david@atomist.com>
2017-03-13 17:05:48 -0500

    XmlType from FileMutableView
    Fixes #420

---
+++ src/test/scala/com/atomist/rug/kind/xml/XmlTypeTest.scala
@@ -0,0 +1,30 @@
+package com.atomist.rug.kind.xml
+
+import com.atomist.rug.kind.core.{FileMutableView, ProjectMutableView}
+import org.scalatest.{FlatSpec, Matchers}
+import com.atomist.rug.kind.java.JavaTypeUsageTest
+import com.atomist.source.{EmptyArtifactSource, SimpleFileBasedArtifactSource, StringFileArtifact}
+
+
+class XmlTypeTest extends FlatSpec with Matchers {
+
+  it should "find all the XML files in a project" in {
+    val pmv = new ProjectMutableView(EmptyArtifactSource(""), JavaTypeUsageTest.NewSpringBootProject)
+    val xt = new XmlType
+    xt.findAllIn(pmv) match {
+      case Some(xs) => assert(xs.size === 4)
+      case x => fail(s"no XMLs found, found $x instead")
+    }
+  }
+
+  it should "find XML in a .xml file" in {
+    val xmlFile = StringFileArtifact("src/resources/royalty.xml", """<prince>Rogers Nelson</prince>""")
+    val fmv = new FileMutableView(xmlFile, null)
+    val xt = new XmlType
+    xt.findAllIn(fmv) match {
+      case Some(xs) => assert(xs.size === 1)
+      case x => fail(s"no XML, found $x")
+    }
+
+  }
+}

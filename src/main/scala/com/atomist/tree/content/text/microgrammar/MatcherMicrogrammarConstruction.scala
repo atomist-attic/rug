8619131a6fd18de9620edc87c69bfd7258c0016a
Jessica Kerr <jessitron@gmail.com>
2017-03-13 21:51:54 -0500

    A first microgrammar constructor (#416)
    * update rug version, use editor
    
    * Implement 'or' matcher constructor
    
    * Impersonal exception message :-(

--- src/main/scala/com/atomist/tree/content/text/microgrammar/MatcherMicrogrammarConstruction.scala
+++ src/main/scala/com/atomist/tree/content/text/microgrammar/MatcherMicrogrammarConstruction.scala
@@ -11,10 +11,28 @@ object MatcherMicrogrammarConstruction {
     val parsedMatcher = matcherParser.parseMatcher(name, grammar)
 
     val knownMatchers: Map[String, Matcher] = submatchers.map {
-      case (name, grammar : String) => (name, matcherParser.parseMatcher(name, grammar))
-    }
+      case (name, matcher) => (name, interpretMatcher(matcher, name))
+      }
 
     new MatcherMicrogrammar(parsedMatcher, name, knownMatchers)
+  }
+
+  def constructOr(properties: Map[String, Any]): Matcher = {
+    val componentProperty = properties.getOrElse("components", throw new RuntimeException("an Or should have a component"))
+    // I have no idea why the array comes in as a map of indices to values but it does
+    val components = componentProperty match {
+      case m: Map[_, _] => m.values.map(interpretMatcher(_))
+      case _ => throw new RuntimeException("expected an array of 'or' components, as a Map[Number, Any]")
+    }
+    components.reduce(_.alternate(_))
+  }
+
+  def interpretMatcher(m: Any, name: String = "anonymous") = m match {
+    case grammar: String => matcherParser.parseMatcher(name, grammar)
+    case micromatcher: Map[String, Any] @unchecked if micromatcher("kind") == "or" =>
+      constructOr(micromatcher.asInstanceOf[Map[String, Any]])
+    case _ => throw new RuntimeException(s"Unrecognized object provided for submatcher $name")
+
   }
 
 }

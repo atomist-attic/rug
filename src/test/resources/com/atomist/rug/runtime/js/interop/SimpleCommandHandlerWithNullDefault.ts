f15eeaea8b542bd80ded2f602cf2f5688e9f1cf1
James Carnegie <james@atomist.com>
2017-03-22 20:02:36 +0000

    Don't set default values to null if they're already null #458

---
+++ src/test/resources/com/atomist/rug/runtime/js/interop/SimpleCommandHandlerWithNullDefault.ts
@@ -0,0 +1,15 @@
+import {HandleCommand, Instruction, Response, HandlerContext, Plan, Message} from '@atomist/rug/operations/Handlers'
+import {CommandHandler, Parameter, Tags, Intent} from '@atomist/rug/operations/Decorators'
+
+@CommandHandler("ShowMeTheKitties","Search Youtube for kitty videos and post results to slack")
+class KittieFetcher implements HandleCommand{
+
+  @Parameter({description: "test", pattern: "@any", required: false})
+  test: string = null
+
+  handle(ctx: HandlerContext) : Message {
+    return new Message();
+  }
+}
+
+export let command = new KittieFetcher();

a9ae2957090603379fb926a8cc3fd1fba84e7531
Rod Johnson <rod@atomist.com>
2017-03-29 16:03:51 +0100

    Added tests for stringifying Nashorn objects

--- src/test/resources/com/atomist/rug/test/gherkin/handler/event/PassingFeature1StepsAgainstGenerated.ts
+++ src/test/resources/com/atomist/rug/test/gherkin/handler/event/PassingFeature1StepsAgainstGenerated.ts
@@ -6,8 +6,15 @@ Given("a sleepy country", f => {
 })
 When("a visionary leader enters", world => {
    world.registerHandler("ReturnsEmptyPlanEventHandlerGen1")
-   world.sendEvent(new Build())
+   let b = new Build()
+   world.sendEvent(b)
+   // Check we don't break with toString problems
+   let stringIt = String(b)
+   let stringItAgain = "" + b
+   let andThis = "" + world
 })
 Then("excitement ensues", world => {
+    "" + world.plan()
+    "" + world.plan().messages
     return world.plan().messages.length == 0
 })

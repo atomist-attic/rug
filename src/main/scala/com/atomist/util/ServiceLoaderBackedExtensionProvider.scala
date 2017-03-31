f0f5d2ea8647a88387fed94c4c33377eed6a9a5a
Christian Dupuis <cd@atomist.com>
2017-03-31 01:03:18 +0100

    Remove caching from service loader registries

--- src/main/scala/com/atomist/util/ServiceLoaderBackedExtensionProvider.scala
+++ src/main/scala/com/atomist/util/ServiceLoaderBackedExtensionProvider.scala
@@ -17,15 +17,8 @@ import scala.reflect._
 class ServiceLoaderBackedExtensionProvider[T: ClassTag](val keyProvider: (T) => String)
   extends LazyLogging {
 
-  // Sharing Rug in a hierarchy of CLassLoaders requires the ServiceLoader to be triggered
-  // for each ClassLoader; using WeakHashMap so ClassLoader references can be garbage collected
-  private val providers = mutable.WeakHashMap[ClassLoader, Map[String, T]]()
-
   def providerMap: Map[String, T] = {
-    providers.get(Thread.currentThread().getContextClassLoader) match {
-      case Some(tm) => tm
-      case _ =>
-        val providersMap: Map[String, T] = ServiceLoader.load(classTag[T].runtimeClass).asScala.map {
+        ServiceLoader.load(classTag[T].runtimeClass).asScala.map {
           case t: T =>
             val key = keyProvider.apply(t)
             logger.info(s"Registered provider '${key}' with class '${t.getClass}'")
@@ -33,9 +26,5 @@ class ServiceLoaderBackedExtensionProvider[T: ClassTag](val keyProvider: (T) => 
           case wtf =>
             throw new RugRuntimeException("Extension", s"Provider class ${wtf.getClass} must implement ${classTag[T].runtimeClass.getName} interface", null)
         }.toMap
-        providers += Thread.currentThread().getContextClassLoader -> providersMap
-        providersMap
     }
-  }
-
 }

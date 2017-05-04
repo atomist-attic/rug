ef607c45e4dfe7addf9cc375f149d2b5ae3c0436
Jessica Kerr <jessitron@gmail.com>
2017-05-05 02:08:04 +1200

    This exposes a bug
    The node returned from the break doesn't have children. It should

--- .atomist/editors/UpgradeRegexParsing.ts
+++ .atomist/editors/UpgradeRegexParsing.ts
@@ -17,30 +17,42 @@ class UpgradeRegexParsing {
     edit(project: Project) {
 
         let mg = new Microgrammar("microgrammarMicrogrammar",
-            `Microgrammar ( "$mgName" , \` $interestingBit $endString $endOfMG`,
+            `Microgrammar ( $mgName , \` $interestingBit $endString $endOfMG`,
             {
                 variableStart: Literal("$"),
-                mgName: Regex("[a-zA-Z_0-9-]+"),
+                mgName: Regex(`["'][a-zA-Z_0-9-]+["']`),
                 interestingBit: SkipAhead(` $variableStart $subName $embeddedRegex `),
                 embeddedRegex: ":§ $regexContent §",
                 subName: Regex("[a-zA-Z_0-9-]+"),
                 regexContent: Regex("[^§]*"),
-                endOfMG: ")",
+                endOfMG: "$closeParen",
+                closeParen: ")",
                 endString: SkipAhead("`")
             });
 
+        // TEST THE MICROGRAMMAR
+        let shouldMatch = `Microgrammar('modelVersion', \` < modelVersion > $version:§[a-zA - Z0 - 9_\\.]+§</modelVersion>\`)`
+        console.log("should match:" + shouldMatch);
+        let result: any = project.context.microgrammarHelper.strictMatch(mg, shouldMatch);
+        // I don't think instanceof is right here; how do I ask, is it a string?
+        console.log("Result:" + result);
+
+        // DO THE THINGS
+
         let eng = project.context.pathExpressionEngine.addType(mg);
 
-        let editMe = '/src/test/scala/com/atomist/rug//microgrammarMicrogrammar()';
+        let editMe = '/src/test/scala/com/atomist//microgrammarMicrogrammar()';
 
-        console.log("doing something");
+        console.log("looking in: " + editMe);
 
         let count = 1;
 
         eng.with<any>(project, editMe, e => {
             console.log("Found one: " + e)
-            e.endOfMg.update(` { ${e.subName.value()}: Regex(${e.regexContent.value()}) } )`)
-            e.embeddedRegex.update('');
+            let submatcherName = e.interestingBit.subName.value();
+            let regexContent = e.interestingBit.embeddedRegex.regexContent.value()
+            e.endOfMG.update(` { ${submatcherName}: Regex(${regexContent}) } )`)
+            e.interestingBit.embeddedRegex.update('');
         });
 
 

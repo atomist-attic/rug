fc6910c52a10069f2e24441d05a1658395de0d74
Jessica Kerr <jessitron@gmail.com>
2017-05-09 19:47:38 -0500

    Move changes to new location

--- src/main/typescript/rug/tree/Microgrammars.ts
+++ src/main/typescript/rug/tree/Microgrammars.ts
@@ -1,12 +1,58 @@
 
-type MicroMatcher = { kind: string } | string;
+type MicroMatcher = { kind: string } | String
 
-const Or = (alternatives: MicroMatcher[]): MicroMatcher => {
-    const or = {
+let Or = function (alternatives: MicroMatcher[]): MicroMatcher {
+    let or = {
         kind: "or",
-        components: alternatives,
+        components: alternatives
     };
     return or;
 };
 
-export { MicroMatcher, Or };
+let Regex = function (javaStyleRegex: string): MicroMatcher {
+    let regex = { kind: "regex", regularExpression: javaStyleRegex };
+    return regex;
+}
+
+let Repeat = function (what: MicroMatcher): MicroMatcher {
+    let repeat = {
+        kind: "repeat",
+        what: what
+    };
+    return repeat;
+}
+
+let Optional = function (what: MicroMatcher): MicroMatcher {
+    let opt = {
+        kind: "optional",
+        what: what
+    };
+    return opt;
+}
+
+let SkipAhead = function (to: MicroMatcher): MicroMatcher {
+    let skipAhead = {
+        kind: "break",
+        to: to
+    };
+    return skipAhead;
+}
+
+/* this one is picky about whitespace */
+let Literal = function (whitespaceSensitive: string): MicroMatcher {
+    let opt = {
+        kind: "strict-literal",
+        content: whitespaceSensitive
+    };
+    return opt;
+}
+
+let Concat = function (components: MicroMatcher[]): MicroMatcher {
+    let concat = {
+        kind: "concat",
+        components: components
+    };
+    return concat;
+};
+
+export { MicroMatcher, Or, Regex, Repeat, Optional, Literal, Concat, SkipAhead }

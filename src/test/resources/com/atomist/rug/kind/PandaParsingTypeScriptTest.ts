8aefc372f6a8a38c32280f8e131f5d7e51c1c8fd
Jessica Kerr <jessitron@gmail.com>
2017-05-21 16:19:59 -0400

    a test for a language extension
    I'm attempting to make this simple
    but it is not. I can work from here on making it
    a little simpler.

---
+++ src/test/resources/com/atomist/rug/kind/PandaParsingTypeScriptTest.ts
@@ -0,0 +1,26 @@
+import { Project, File } from '@atomist/rug/model/Core'
+import { Editor } from '@atomist/rug/operations/Decorators'
+import { PathExpressionEngine, TextTreeNode } from '@atomist/rug/tree/PathExpression'
+import { Parameter } from "@atomist/rug/operations/Decorators";
+import { Pattern } from "@atomist/rug/operations/RugOperation";
+
+@Editor("PandaParsingTypeScriptTest", "Uses PandaParsing from TypeScript")
+class PandaParsingTypeScriptTest {
+
+    @Parameter({ pattern: Pattern.any })
+    changePandasInCurliesTo: string;
+
+    edit(project: Project) {
+
+        let eng : PathExpressionEngine = project.context.pathExpressionEngine;
+
+        let pandafile = eng.scalar<Project, File>(project, `/my.panda`);
+        let parsedPanda = eng.scalar<File, TextTreeNode>(pandafile, "/Panda()");
+
+        eng.with<TextTreeNode>(parsedPanda, `/curly/word`, pandaWord => {
+            pandaWord.update(this.changePandasInCurliesTo);
+        })
+
+    }
+}
+export let editor = new PandaParsingTypeScriptTest();

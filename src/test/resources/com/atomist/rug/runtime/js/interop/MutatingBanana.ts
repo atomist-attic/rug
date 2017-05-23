e5d2805da16dc20abc79b2145e0a3e5d46041273
Jessica Kerr <jessitron@gmail.com>
2017-05-22 22:43:48 -0500

    Restore dynamic js grammar tests
    These used microgrammars only as comments

---
+++ src/test/resources/com/atomist/rug/runtime/js/interop/MutatingBanana.ts
@@ -0,0 +1,85 @@
+import {Project, File} from '@atomist/rug/model/Core'
+import {Editor} from '@atomist/rug/operations/Decorators'
+import {PathExpression,TreeNode,TypeProvider} from '@atomist/rug/tree/PathExpression'
+import {PathExpressionEngine} from '@atomist/rug/tree/PathExpression'
+import {Match} from '@atomist/rug/tree/PathExpression'
+import {Parameter} from '@atomist/rug/operations/RugOperation'
+
+
+class FruitererType implements TypeProvider {
+
+ typeName = "fruiterer"
+
+ private isFile(f: TreeNode): f is File {
+   return true
+ }
+
+ find(context: TreeNode): TreeNode[] {
+   if (this.isFile(context)) {
+     let f = context as File
+     if (f.isJava)
+      return [ new Fruiterer(f) ]
+      else return []
+   }
+   else 
+    return []
+ }
+}
+
+class Fruiterer implements TreeNode {
+
+  constructor(public file: File) {}
+
+  parent() { return this.file }
+
+  nodeName(): string { return "fruiterer" }
+
+  nodeTags(): string[] { return [ this.nodeName()] }
+
+  children() { return [ new MutatingBanana(this.file), new Pear() ] }
+
+}
+
+class MutatingBanana implements TreeNode {
+
+  constructor(public file: File) {}
+
+  parent() { return this.file }
+
+  nodeName(): string { return "mutatingBanana" }
+
+  nodeTags(): string[] { return [ this.nodeName()] }
+
+  children() { return [] }
+
+  mutate(): void { 
+    this.file.prepend("I am evil")
+  }
+}
+
+class Pear implements TreeNode {
+
+  parent() { return null }
+
+  nodeName(): string { return "pear" }
+
+  nodeTags(): string[] { return [ this.nodeName()] }
+
+  children() { return [] }
+
+}
+
+@Editor("Constructed", "Uses two dynamic grammars")
+class TwoLevel  {
+
+    edit(project: Project) {
+      let mg = new FruitererType()
+      let eng: PathExpressionEngine = project.context.pathExpressionEngine.addType(mg)
+
+      eng.with<MutatingBanana>(project, "//File()/fruiterer()/mutatingBanana()", n => {
+        n.mutate()
+      })
+    }
+  }
+
+export let editor = new TwoLevel();
---
+++ src/test/resources/com/atomist/rug/runtime/js/interop/TwoLevel.ts
@@ -0,0 +1,94 @@
+import {Project, File} from '@atomist/rug/model/Core'
+import {Editor} from '@atomist/rug/operations/Decorators'
+import {PathExpression,TreeNode,TypeProvider} from '@atomist/rug/tree/PathExpression'
+import {PathExpressionEngine} from '@atomist/rug/tree/PathExpression'
+import {Match} from '@atomist/rug/tree/PathExpression'
+import {Parameter} from '@atomist/rug/operations/RugOperation'
+
+
+class FruitererType implements TypeProvider {
+
+ typeName = "fruiterer"
+
+ private isFile(f: TreeNode): f is File {
+   return true
+ }
+
+ find(context: TreeNode): TreeNode[] {
+   if (this.isFile(context)) {
+     let f = context as File
+     //console.log(f.name())
+     if (f.isJava)
+      return [ new Fruiterer() ]
+      else return []
+   }
+   else 
+    return []
+ }
+
+}
+
+class Fruiterer implements TreeNode {
+
+  parent() { return null}
+
+  nodeName(): string { return "fruiterer" }
+
+  nodeTags(): string[] { return [ this.nodeName()] }
+
+  value(): string { return "" }
+
+  update(newValue: string) {}
+
+  children() { return [ new Banana(), new Pear() ] }
+
+}
+
+class Banana implements TreeNode {
+
+  parent() { return null}
+
+  nodeName(): string { return "banana" }
+
+  nodeTags(): string[] { return [ this.nodeName()] }
+
+  value(): string { return "yellow" }
+
+  children() { return [] }
+
+}
+
+class Pear implements TreeNode {
+
+  parent() { return null}
+
+  nodeName(): string { return "pear" }
+
+  nodeTags(): string[] { return [ this.nodeName()] }
+
+  value(): string { return "green" }
+
+  children() { return [] }
+
+}
+
+@Editor("Users single microgrammar")
+class TwoLevel  {
+
+    edit(project: Project) {
+      //console.log("Editing")
+      let mg = new FruitererType()
+      let eng: PathExpressionEngine = project.context.pathExpressionEngine.addType(mg)
+
+      let i = 0
+      eng.with<Banana>(project, "//File()/fruiterer()/banana()", n => {
+        //console.log("Checking color of banana")
+        if (n.value() != "yellow")
+         throw new Error(`Banana is not yellow but [${n.value()}]. Sad.`)
+        i++
+      })
+      if (i != 2)
+       throw new Error(`Two bananas should have been tested, not ${i}. Sad.`)
+    }
+  }
+export let editor = new TwoLevel();

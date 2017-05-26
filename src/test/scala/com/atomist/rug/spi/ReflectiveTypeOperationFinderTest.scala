d9c253822593895f3e8a76226c8d5c6d3a154dad
Jessica Kerr <jessitron@gmail.com>
2017-05-25 21:46:47 -0500

    Now the test is valid

--- src/test/scala/com/atomist/rug/spi/ReflectiveTypeOperationFinderTest.scala
+++ src/test/scala/com/atomist/rug/spi/ReflectiveTypeOperationFinderTest.scala
@@ -6,15 +6,12 @@ import org.scalatest.{FlatSpec, Matchers}
 class ReflectiveTypeOperationFinderTest extends FlatSpec with Matchers {
 
   it should "find well known operations on well-known types" in {
-    val st = DefaultTypeRegistry.findByName("YamlFile").get
+    val st = DefaultTypeRegistry.findByName("Project").get
     st.allOperations.find(_.name == "children") shouldBe defined
   }
 
-  it should "not find property on parent" in pendingUntilFixed {
-    val st = DefaultTypeRegistry.findByName("YamlFile").get
-
-    println(s"There are ${st.operations.length} operations")
-    println(s"There are ${st.allOperations.length} allOperations")
+  it should "not find property on parent" in {
+    val st = DefaultTypeRegistry.findByName("Project").get
 
     st.operations.find(_.name == "children") shouldBe empty
   }
